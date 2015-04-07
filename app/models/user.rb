39ba7d715101bcb08cc4755a79ba1cb5bfc127bd
Jian Weihang <tonytonyjan@gmail.com>
2015-04-07 14:44:19 +0800

    rails g devise user

---
+++ app/models/user.rb
@@ -0,0 +1,20 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+  # scopes
+
+  # constants
+
+  # attributes
+
+  # associations
+
+  # validations
+
+  # callbacks
+
+  # other
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root 'pages#home'
+
+  devise_for :users
+
   namespace :admin do
     root action: :home
   end
