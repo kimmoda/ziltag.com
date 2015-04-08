cbf6144ec68dac618e9b7f1907a8af42255fbbc9
Jian Weihang <tonytonyjan@gmail.com>
2015-04-08 21:14:31 +0800

    新增名稱空間

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,3 +1,3 @@
 class AdminController < ApplicationController
-  http_basic_authenticate_with Settings.http_basic_authentication.symbolize_keys unless Rails.env.development?
+  http_basic_authenticate_with Settings.admin.http_basic_authentication.symbolize_keys unless Rails.env.development?
 end
