523fb626196ceb98dde819abc13ac9a36da3f9a7
Jian Weihang <tonytonyjan@gmail.com>
2015-04-16 00:18:55 +0800

    預防 nil

--- app/inputs/select2_input.rb
+++ app/inputs/select2_input.rb
@@ -3,9 +3,8 @@ class Select2Input < SimpleForm::Inputs::CollectionSelectInput
     label_method, value_method = detect_collection_methods
     input_html_options.deep_merge! data: {select2_resource: @reflection.table_name}
     merged_input_options = merge_wrapper_options(input_html_options, wrapper_options)
-
     @builder.collection_select(
-      attribute_name, [object.send(@reflection.name)], value_method, label_method,
+      attribute_name, Array(object.send(@reflection.name)), value_method, label_method,
       input_options, merged_input_options
     )
   end
