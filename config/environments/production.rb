bd9335e8ccbeb5006a6e78ffbc273d589258f7cc
Jian Weihang <tonytonyjan@gmail.com>
2015-04-16 00:07:59 +0800

    修正 mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,12 +77,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = {host: ENV['HOST'] || 'ziltag.com'}
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address: ENV['SMTP_ADDRESS'],
-    port: ENV['SMTP_PORT'] || 587,
-    user_name: ENV['SMTP_USER_NAME'],
-    password: ENV['SMTP_PASSWORD']
-  }
+  Settings.action_mailer.each do |key, value|
+    config.action_mailer.send("#{key}=", value.symbolize_keys)
+  end
 end
