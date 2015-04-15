f70bbe89c789520719b7f70d0e9c2a504f32361f
Jian Weihang <tonytonyjan@gmail.com>
2015-04-15 21:17:12 +0800

    設定郵件伺服器

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,4 +78,11 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.default_url_options = {host: 'ziltag.com'}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: ENV['SMTP_ADDRESS'],
+    port: ENV['SMTP_PORT'] || 587,
+    user_name: ENV['SMTP_USER_NAME'],
+    password: ENV['SMTP_PASSWORD']
+  }
 end
