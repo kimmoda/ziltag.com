e77dd8072aefa1225ca54faf842dd04dd2e00571
Jian Weihang <tonytonyjan@gmail.com>
2015-04-23 15:21:15 +0800

    預設頭像

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -4,4 +4,8 @@ class AvatarUploader < CarrierWave::Uploader::Base
   def store_dir
     "uploads/#{model.class.table_name}/#{mounted_as}/#{model.id}"
   end
+
+  def default_url
+    '/images/fallback/' << [version_name, 'guest.png'].compact.join('_')
+  end
 end
