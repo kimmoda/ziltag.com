ebcda1b0cef344220ecf2f046e4162076ab41e70
Jian Weihang <tonytonyjan@gmail.com>
2015-04-23 19:30:16 +0800

    讓使用者可以用 username 或 email 登入

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ActiveRecord::Base
   # constants
 
   # attributes
+  attr_accessor :login
   mount_uploader :avatar, AvatarUploader
 
   # associations
@@ -16,11 +17,21 @@ class User < ActiveRecord::Base
   has_many :comments, primary_key: :email, foreign_key: :email
 
   # validations
-  validates :email, :username, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+  validates :username, presence: true, uniqueness: {case_sensitive: false}
 
   # callbacks
 
   # other
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    ret = if login = conditions.delete(:login)
+      where(conditions.to_hash).where(["lower(username) = lower(:value) OR lower(email) = :value", { :value => login.downcase }]).first
+    else
+      where(conditions.to_hash).first
+    end
+    ret
+  end
 
   def to_s
     email
