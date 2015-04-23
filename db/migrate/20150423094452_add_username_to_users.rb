30d2f7752c2701a18bf85661ed8b197c1755fb14
Jian Weihang <tonytonyjan@gmail.com>
2015-04-23 18:04:18 +0800

    加上 username

---
+++ db/migrate/20150423094452_add_username_to_users.rb
@@ -0,0 +1,12 @@
+class AddUsernameToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :username, :string
+    User.find_each do |user|
+      username = user.email[/^(\w+)@/, 1]
+      username = SecureRandom.hex(3) while User.exists?(username: username)
+      user.update_attribute :username, username
+    end
+    change_column_null :users, :username, false
+    add_index :users, :username, unique: true
+  end
+end
