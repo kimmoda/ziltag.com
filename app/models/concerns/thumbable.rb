a4a898ac177ff7782b416fdab247f55addd74b01
Jian Weihang <tonytonyjan@gmail.com>
2015-05-07 11:44:57 +0800

    Revert issue #8 and resolved #16"
    This reverts commit b2834c303465acc105afb56f071857a7a132703c.

--- app/models/concerns/thumbable.rb
+++ app/models/concerns/thumbable.rb
@@ -2,9 +2,24 @@ module Thumbable
   extend ActiveSupport::Concern
 
   included do
-    include CarrierWave::Vips
+    include CarrierWave::MiniMagick
     version :thumb do
-      process resize_to_fill: [50, 50]
+      process resize_to_fill_space: [50, 50]
+    end
+  end
+
+  # TODO resize_to_fill 在 graphicsmagick 上會有問題
+  # https://github.com/carrierwaveuploader/carrierwave/issues/1520
+  def resize_to_fill_space(width, height, gravity = 'Center')
+    manipulate! do |image|
+      MiniMagick::Tool::Convert.new do |convert|
+        convert.thumbnail "#{width}x#{height}^"
+        convert.gravity gravity
+        convert.extent "#{width}x#{height}"
+        convert << image.path # input
+        convert << image.path # output
+      end
+      image
     end
   end
 
