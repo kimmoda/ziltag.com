8e6fdd2d2f0384ca55f250cc060874c6a1e12c29
Jian Weihang <tonytonyjan@gmail.com>
2015-05-09 15:34:37 +0800

    resolve #29

---
+++ app/helpers/simple_form_helper.rb
@@ -0,0 +1,13 @@
+module SimpleFormHelper
+  def vertical_form resource, options = {}, &block
+    options[:wrapper] = :vertical_form
+    options[:html] = {class: nil}
+    options[:wrapper_mappings] = {
+      check_boxes: :vertical_radio_and_checkboxes,
+      radio_buttons: :vertical_radio_and_checkboxes,
+      file: :vertical_file_input,
+      boolean: :vertical_boolean
+    }
+    simple_form_for resource, options, &block
+  end
+end
