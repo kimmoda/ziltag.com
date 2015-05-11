374884315819aa052cfba4a08c2f501245a5ee4a
Carlos Chang <chang.abin@gmail.com>
2015-05-12 00:08:35 +0800

    樂貼更新

--- app/assets/javascripts/application/components/ZiltagSticker.js
+++ app/assets/javascripts/application/components/ZiltagSticker.js
@@ -4,29 +4,29 @@ var ZiltagSticker = {
     y: 0
   },
   
-  
+  move_direction: "",
   default_width: 300,
+  default_height: 300,
   
   appendImg: function(){
     var that = this;
     $("article[data-ziltag-sticker-article]").each(function(){
       
       // Step1：圖片寬、高比較大小
-      var move_direction;
       if( that.compareSide(parseInt($(this).outerWidth()), parseInt($(this).outerHeight())) ){
         
         // Step2：縮放的比例
         var ratio = that.calculateRatio( $(this).children("img[data-show-sticker]").width() );
         $(this).children("img[data-show-sticker]").css("width", that.default_width);
         
-        move_direction = "vertical";
+        that.move_direction = "vertical";
       }else{
       
         // Step2：縮放的比例
         var ratio = that.calculateRatio( $(this).children("img[data-show-sticker]").height() );
         $(this).children("img[data-show-sticker]").css("height", that.default_width);
         
-        move_direction = "horizontal";
+        that.move_direction = "horizontal";
       }
       
       that.place.x = $(this).children("img[data-show-sticker]").data("ziltag-x") * ratio;
@@ -38,16 +38,39 @@ var ZiltagSticker = {
       that.stickerPos( $(this) );
       
       // Step4：移動圖片
-      if( move_direction == "vertical" ){
-        var move = that.place.y - that.default_width;
+      if( that.move_direction == "vertical" ){
+        var move = that.place.y - that.default_height;
         if( move >= 0){
-          $(this).css({
-            top: "-" + ((that.default_width/2) + move) + "px"
-          });
+          
+          if( (parseInt($(this).outerHeight()) - that.place.y) <= that.default_height){
+            $(this).css({
+              bottom: "0px"
+            });
+          }else{
+            $(this).css({
+              top: "-" + ((that.default_height/2) + move) + "px"
+            });
+          }
+          
         }
       }
       if( move_direction == "horizontal" ){
-        
+        var move = that.place.x - that.default_width;
+        if( move >= 0){
+          
+          
+          if( (parseInt($(this).outerWidth()) - that.place.x) <= that.default_width){
+            $(this).css({
+              right: "0px"
+            });
+          }else{
+            $(this).css({
+              left: "-" + ((that.default_width/2) + move) + "px"
+            });
+          }
+          
+          
+        }
       }
       
       
@@ -65,7 +88,10 @@ var ZiltagSticker = {
   
   // 計算比例
   calculateRatio: function(side_length){
-    return (this.default_width / side_length);
+    if( this.move_direction == "vertical" ){
+      return (this.default_width / side_length);
+    }
+    return (this.default_height / side_length);
   },
   
   // 樂貼位置
