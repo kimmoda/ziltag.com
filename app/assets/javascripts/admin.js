244eacb89c9f6308021a85ed86d60b64121577ca
Jian Weihang <tonytonyjan@gmail.com>
2015-05-12 14:32:55 +0800

    安裝 redactor

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -4,4 +4,6 @@
 //= require bootstrap
 //= require select2
 //= require URIjs
+//= require redactor
+//= require redactor/zh_tw.js
 //= require_tree ./admin
---
+++ app/assets/javascripts/admin/redactor_helper.coffee
@@ -0,0 +1,3 @@
+$(document).on 'ready page:loda', () ->
+  $('[data-redactor]').redactor
+    lang: 'zh_tw'
