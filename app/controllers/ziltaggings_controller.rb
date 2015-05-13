25b9981756cda99d956e70aeb3c5dfc89cf62a4d
Jian Weihang <tonytonyjan@gmail.com>
2015-05-13 16:24:51 +0800

    顯示 JSON，並在第一個樂貼上產生 10 個文章資料

--- app/controllers/ziltaggings_controller.rb
+++ app/controllers/ziltaggings_controller.rb
@@ -1,4 +1,9 @@
 class ZiltaggingsController < ApplicationController
   def index
   end
+
+  def show
+    @ziltagging = Ziltagging.find params[:id]
+    @other_ziltaggings = Ziltagging.where(image_url: @ziltagging.image_url).where.not(id: params[:id])
+  end
 end
