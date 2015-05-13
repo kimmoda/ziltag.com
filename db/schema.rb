ba8577044640ef1e3bcd29c6a06c54048c47b0c5
Jian Weihang <tonytonyjan@gmail.com>
2015-05-14 02:17:55 +0800

    刪除無用的資料表 [ci skip]

--- db/schema.rb
+++ db/schema.rb
@@ -53,12 +53,6 @@ ActiveRecord::Schema.define(version: 20150511161428) do
 
   add_index "posts", ["user_id"], name: "index_posts_on_user_id", using: :btree
 
-  create_table "students", force: :cascade do |t|
-    t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
