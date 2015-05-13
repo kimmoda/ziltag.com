f80bafe885870384b21b3139c80cb30395506cb6
Jian Weihang <tonytonyjan@gmail.com>
2015-05-13 21:10:12 +0800

    置換成資料庫中的圖片

--- app/controllers/ziltaggings_controller.rb
+++ app/controllers/ziltaggings_controller.rb
@@ -1,7 +1,4 @@
 class ZiltaggingsController < ApplicationController
-  def index
-  end
-
   def show
     @ziltagging = Ziltagging.find params[:id]
     @other_ziltaggings = Ziltagging.where(image_url: @ziltagging.image_url).where.not(id: params[:id])
