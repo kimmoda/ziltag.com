00d5975fd90f4d707ff6fdf8a4bc0fed2184fb39
Carlos Chang <chang.abin@gmail.com>
2015-05-13 11:47:20 +0800

    更新

--- app/assets/javascripts/application/partials/ziltag_article.js
+++ app/assets/javascripts/application/partials/ziltag_article.js
@@ -6,6 +6,7 @@ $(document).on('ready page:load', function(){
         $(this).closest("div").find("ul[data-msg-list] > li[data-not-first-item]").slideToggle();
         $(this).closest("div").find("ul[data-msg-list] > li[data-first-item]").toggleClass("add_bottom_line");
         $(this).find("i.fa").toggleClass("hidden");
+        //allJs.tune_layout();
       });
     });
   }
