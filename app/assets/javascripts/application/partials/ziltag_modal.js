13bf1c18191169a88d3ae81a1b666b3c353b265a
Carlos Chang <chang.abin@gmail.com>
2015-05-18 18:28:01 +0800

    resolve #66

--- app/assets/javascripts/application/partials/ziltag_modal.js
+++ app/assets/javascripts/application/partials/ziltag_modal.js
@@ -12,29 +12,7 @@ $(document).on('ready page:load', function(){
       allJs.data_ziltag_modal_layout_back($(this));
     });
     
-    $('[data-ziltag-modal]').on('ajax:success', function(event, data){
-      window.history.pushState(null, null, data.link);
-      
-      $("[data-user-username]").html(data.user.username);
-      $("[data-user-avatar]").attr("src", data.user.avatar);
-      $("[data-post-title]").html(data.post.title);
-      $("[data-post-content]").html(data.post.content);
-      $("[data-dreated-at]").html(data.created_at);
-      $("[data-modal-post-ziltag-article]").find("[data-show-sticker]").attr("data-ziltag-x", data.x).attr("data-ziltag-y", data.y);
-      $("[data-location-name]").html(data.location.name);
-      $("[data-location-address]").html(data.location.address);
-      
-      $("[data-image_url]").attr("src", data.image_url);
-      
-      // 貼上sticker
-      ZiltagSticker.appendImgOnModal();
-      ZiltagSticker.appendOtherZiltags($("article[data-ziltag-sticker-article][data-is-modal='true']"), JSON.stringify(data.other_ziltaggings));
-      
-      // 動態從右至左跑出
-      allJs.data_ziltag_modal_layout_after_click();
-      
-      
-    });
+    
     
   }
   
