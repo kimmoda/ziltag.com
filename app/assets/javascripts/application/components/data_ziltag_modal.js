f727f859ad0d7b7a42985694813aac90d109abd5
Carlos Chang <chang.abin@gmail.com>
2015-05-20 17:29:48 +0800

    Front page-以圖找貼 #5

--- app/assets/javascripts/application/components/data_ziltag_modal.js
+++ app/assets/javascripts/application/components/data_ziltag_modal.js
@@ -19,7 +19,7 @@ $(document).on('ready page:load', function(){
     
     // 貼上sticker
     ZiltagSticker.appendImgOnModal();
-    ZiltagSticker.appendOtherZiltags($("article[data-ziltag-sticker-article][data-is-modal='true']"), JSON.stringify(data.other_ziltaggings));
+    ZiltagSticker.appendOtherZiltags($("article[data-ziltag-sticker-article][data-is-modal='true']"), JSON.stringify(data.other_ziltaggings), false);
     
     // 動態從右至左跑出
     allJs.data_ziltag_modal_layout_after_click();
