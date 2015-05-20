162623d3f69d9de8b4534913068bbfff7988dc60
Jian Weihang <tonytonyjan@gmail.com>
2015-05-21 05:22:47 +0800

    預載圖片 [#76]

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
   def home
-    @ziltaggings = Ziltagging.includes(post: :user).order('id DESC').limit(10)
+    @ziltaggings = Ziltagging.includes(:photo, post: :user).order('id DESC').limit(10)
   end
 end
