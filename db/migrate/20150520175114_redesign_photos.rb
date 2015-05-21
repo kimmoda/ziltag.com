18da321c88f39636786de2bb266fe44bb26af618
Jian Weihang <tonytonyjan@gmail.com>
2015-05-21 17:55:39 +0800

    讀取原始資料

--- db/migrate/20150520175114_redesign_photos.rb
+++ db/migrate/20150520175114_redesign_photos.rb
@@ -6,9 +6,9 @@ class RedesignPhotos < ActiveRecord::Migration
     add_reference :ziltaggings, :photo, index: true, foreign_key: true
     add_reference :comments, :photo, index: true, foreign_key: true
 
-    Photo.find_each{ |photo| photo.update_column :source, photo.url }
-    Ziltagging.find_each{ |ziltagging| ziltagging.update_column :photo_id, Photo.find_by!(url: ziltagging.image_url).id }
-    Comment.find_each { |comment| comment.update_column :photo_id, Photo.find_by!(url: comment.image_url).id }
+    Photo.find_each{ |photo| photo.update_column :source, photo.read_attribute_before_type_cast(:url) }
+    Ziltagging.find_each{ |ziltagging| ziltagging.update_column :photo_id, Photo.find_by!(url: ziltagging.read_attribute_before_type_cast(:image_url)).id }
+    Comment.find_each { |comment| comment.update_column :photo_id, Photo.find_by!(url: comment.read_attribute_before_type_cast(:image_url)).id }
 
     change_column_null :ziltaggings, :photo_id, false
     change_column_null :comments, :photo_id, false
