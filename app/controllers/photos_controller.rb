9fb4fbe12651849d5d06c4e94753ad38c29390e5
Jian Weihang <tonytonyjan@gmail.com>
2015-05-21 18:04:48 +0800

    fixed [#103]

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,5 +1,5 @@
 class PhotosController < ApplicationController
   def index
-    @photos = Photo.includes(:user, ziltaggings: [:post]).limit(10)
+    @photos = Photo.includes(:user, ziltaggings: [:post]).order('created_at DESC').limit(10)
   end
 end
