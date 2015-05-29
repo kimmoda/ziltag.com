d45fd1fe7f49a86f0aebb949592419136a603855
Jian Weihang <tonytonyjan@gmail.com>
2015-05-30 01:36:39 +0800

    嘗試用簡短的方式解決 ziltag modal 的對其問題。

---
+++ app/assets/javascripts/application/ziltag_modal.js
@@ -0,0 +1,10 @@
+main(function(){
+  var modal = document.getElementById('ziltag_modal');
+  var section_post = document.querySelector('[data-layout="col_right"]');
+  function show(){
+    var rect = section_post.getBoundingClientRect();
+    modal.style.left = rect.left + 'px';
+    return modal.style.width = document.body.clientWidth - rect.left + 'px';
+  };
+  window.addEventListener('resize', show);
+});
