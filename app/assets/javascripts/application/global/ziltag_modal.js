4ea72337d07e7dc887f57858fa7bcf612460ab8a
Carlos Chang <chang.abin@gmail.com>
2015-05-29 22:57:31 +0800

    動態增加ziltag [#130]

--- app/assets/javascripts/application/global/ziltag_modal.js
+++ app/assets/javascripts/application/global/ziltag_modal.js
@@ -16,13 +16,15 @@ $(document).on('ready page:load', function(){
     
     // TODO: 重複點擊相同的文章，會重複發送請求索取 JSON 資料，可以在第一次發送時就 cache 結果，減輕伺服器負擔。
     $('[data-ziltag-modal]').on('ajax:success', function(event, data){
-      
+    
       // 置換內容
       ZiltagSticker.replaceModalData(data);
       
       // 貼上sticker
-      ZiltagSticker.appendImgOnModal();
-      ZiltagSticker.appendOtherZiltags($("article[data-ziltag-sticker-article][data-is-modal='true']"), JSON.stringify(data.other_ziltaggings), true, false, false);
+      //ZiltagSticker.appendImgOnModal();
+      //ZiltagSticker.appendOtherZiltags($("article[data-ziltag-sticker-article][data-is-modal='true']"), JSON.stringify(data.other_ziltaggings), true, false, false);
+      
+      ZiltagSticker.appendImgOnModal(data);
       
       // 動態從右至左跑出
       allJs.data_ziltag_modal_layout_after_click();
@@ -33,11 +35,12 @@ $(document).on('ready page:load', function(){
 });
 
 $(window).load(function(){
-  
+  /*
   if( $("[data-ziltag-modal]").length > 0 ){
     // 貼上sticker
     ZiltagSticker.appendImgOnModal();
   }
+  */
   
 });
 
@@ -45,11 +48,12 @@ $(window).resize(function(){
   
   // 滑出視窗的版面控制
   allJs.data_ziltag_modal_layout();
-  
+  /*
   if( $("[data-ziltag-modal]").length > 0 ){
     // 貼上sticker
     ZiltagSticker.appendImgOnModal();
   }
+  */
   
 });
 
