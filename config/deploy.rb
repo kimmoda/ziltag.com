ca8513e40bee716015f38836fc2b7f6fb187ba0d
Jian Weihang <tonytonyjan@gmail.com>
2015-05-31 23:57:16 +0800

    在 precompile assets 之後產生 embedded 檔 [#131]

--- config/deploy.rb
+++ config/deploy.rb
@@ -38,7 +38,7 @@ set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', '
 namespace :deploy do
   after :publishing, :restart
 
-  after 'assets:precompile' do
+  after 'assets:precompile', 'assets:embedded' do
     on release_roles(fetch(:assets_roles)) do
       within release_path do
         with rails_env: fetch(:rails_env) do
