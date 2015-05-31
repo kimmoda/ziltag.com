88abbb9d72d203087598c90ac853afe0f7d52f77
Jian Weihang <tonytonyjan@gmail.com>
2015-06-01 03:45:05 +0800

    修正 admin layout

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,3 +1,4 @@
 class AdminController < ApplicationController
+  layout 'admin'.freeze
   http_basic_authenticate_with Settings.admin.http_basic_authentication.symbolize_keys if Rails.env.production?
 end
