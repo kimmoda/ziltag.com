a81a0b94284cd5be405c2a2c9b28ca8dcb113441
Jian Weihang <tonytonyjan@gmail.com>
2015-05-31 15:51:14 +0800

    加上 model 測試 [#131]

--- test/models/photo_test.rb
+++ test/models/photo_test.rb
@@ -8,4 +8,10 @@ class PhotoTest < ActiveSupport::TestCase
   test '#posts' do
     assert_equal photos(:tony).posts, [posts(:tony)]
   end
+
+  test '::search_by_urls' do
+    ids = Photo.search_by_urls(%w[http://localhost:3000/uploads/photos/image/2/tony.jpg http://localhost:3000/uploads/photos/image/3/tony.jpg]).pluck(:id)
+    assert_equal photos(:tony_2, :david).map!(&:id).sort!, ids.sort!
+  end
+
 end
