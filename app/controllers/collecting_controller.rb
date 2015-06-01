ba59c99ccadc405c0c633bbab401ffb658f1f02c
Jian Weihang <tonytonyjan@gmail.com>
2015-06-01 20:16:54 +0800

    改寫 controller 需要的參數 [#129]

--- app/controllers/collecting_controller.rb
+++ app/controllers/collecting_controller.rb
@@ -1,14 +1,26 @@
 class CollectingController < ApplicationController
-  before_action :authenticate_user!
+  WHITE_TYPE_LIST = %w[Post]
+
+  before_action :authenticate_user!, :set_collectable
 
   def collect
-    current_user.collect! Post.find(params[:post_id])
+    current_user.collect! @collectable
     head :ok
   end
 
   def uncollect
-    current_user.uncollect! Post.find(params[:post_id])
+    current_user.uncollect! @collectable
     head :ok
+  end
+
+private
+
+  def set_collectable
+    if params[:id] && WHITE_TYPE_LIST.include?(params[:type])
+      @collectable = params[:type].constantize.find(params[:id])
+    else
+      head :bad_request
+    end
   end
 
 end
--- test/controllers/collecting_controller_test.rb
+++ test/controllers/collecting_controller_test.rb
@@ -11,7 +11,7 @@ class CollectingControllerTest < ActionController::TestCase
   test "should post collect" do
     refute @david.collect?(posts(:tony))
     sign_in @david
-    post :collect, post_id: posts(:tony).id
+    post :collect, id: posts(:tony).id, type: :Post
     @david.reload
     assert @david.collect?(posts(:tony))
     assert_response :success
@@ -20,10 +20,18 @@ class CollectingControllerTest < ActionController::TestCase
   test "should delete uncollect" do
     assert @tony.collect?(posts(:david))
     sign_in @tony
-    delete :uncollect, post_id: posts(:david).id
+    delete :uncollect, id: posts(:david).id, type: :Post
     @tony.reload
     refute @tony.collect?(posts(:david))
     assert_response :success
+  end
+
+  test "white list" do
+    sign_in @david
+    post :collect, id: posts(:tony).id, type: :Post
+    assert_response :success
+    post :collect, id: users(:tony).id, type: :User
+    assert_response :bad_request
   end
 
 end
