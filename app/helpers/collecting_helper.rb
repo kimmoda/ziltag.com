eafb8ca2479cbb81b0255df9915a1c8bfbe0d8ae
Jian Weihang <tonytonyjan@gmail.com>
2015-06-03 01:55:27 +0800

    簡化收藏按鈕部分的程式碼，降低耦合度

--- app/helpers/collecting_helper.rb
+++ app/helpers/collecting_helper.rb
@@ -1,17 +1,28 @@
 module CollectingHelper
+  def collecting_link record, dom_class
+    case dom_class
+    when :collect
+      link_method = :post
+      link_style = current_user.collect?(record) ? :'display:none' : nil
+    when :uncollect then :delete
+      link_method = :delete
+      link_style = current_user.collect?(record) ? nil : :'display:none'
+    end
+
+    link_to(fa(:star), collect_path(id: record.id, type: record.class.name),
+      data: {class: dom_id(record)},
+      class: "#{dom_class} #{dom_id(record)}",
+      method: link_method,
+      remote: true,
+      style: link_style
+    )
+  end
+
   def collecting_button record
     return unless user_signed_in?
     ret = ActiveSupport::SafeBuffer.new
-    ret << link_to(fa(:star), collect_path(id: record.id, type: record.class.name),
-      data: {class: dom_id(record)}, class: "collect #{dom_id(record)}",
-      method: :post, remote: true,
-      style: (:'display:none' if current_user.collect? record)
-    )
-    ret << link_to(fa(:star), uncollect_path(id: record.id, type: record.class.name),
-      data: {class: dom_id(record)}, class: "uncollect #{dom_id(record)}",
-      method: :delete, remote: true,
-      style: (:'display:none' unless current_user.collect? record)
-    )
+    ret << collecting_link(record, :collect)
+    ret << collecting_link(record, :uncollect)
   end
 
 end
