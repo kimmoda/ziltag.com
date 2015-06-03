a0b3c3eb50e190dbd7ce89ba3c062af51704a4d1
Jian Weihang <tonytonyjan@gmail.com>
2015-06-04 02:57:00 +0800

    點擊的樂貼會變紅，且本來紅色的樂貼會變灰 [#140]

--- app/assets/javascripts/application/ziltag_replace.js
+++ app/assets/javascripts/application/ziltag_replace.js
@@ -3,5 +3,9 @@
 main(function(){
   $(document).on('ajax:success', '[data-ziltag-replace]', function(event, data){
     ZiltagSticker.replaceModalData(data);
+    this.classList.add('active');
+    other_a_tags = this.parentNode.getElementsByTagName('a');
+    for(var i = 0; i < other_a_tags.length; i++)
+      if(other_a_tags[i] !== this) other_a_tags[i].classList.remove('active');
   });
 });
