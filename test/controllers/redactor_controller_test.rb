486102dfeb1a285269f7a58a51e71b502dff5adc
Jian Weihang <tonytonyjan@gmail.com>
2015-06-13 20:37:29 +0800

    修正測試 [#135]

--- test/controllers/redactor_controller_test.rb
+++ test/controllers/redactor_controller_test.rb
@@ -4,6 +4,7 @@ class RedactorControllerTest < ActionController::TestCase
   test 'upload image' do
     post :images, file: fixture_file_upload('images/1.jpg', 'image/jpg')
     assert_response :success
-    assert_equal "{\"filelink\":\"http://localhost:3000/uploads/photos/image/339078013/1.jpg\"}", response.body
+    json = JSON.parse response.body
+    assert_match %r{http://localhost:3000/uploads/photos/image/\d+/1.jpg}, json['filelink']
   end
 end
