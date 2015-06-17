4483d8a5ef5fd6c228b800098431b0b8f3a482d3
Jian Weihang <tonytonyjan@gmail.com>
2015-06-18 06:56:54 +0900

    修正測試 [#135]

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -8,17 +8,7 @@ class PostsControllerTest < ActionController::TestCase
     assert_response :success
     json = JSON.parse response.body
     json.delete 'created_on'
-    expected = {
-      "id"=>339078012,
-      "title"=>"大兜的測試文章",
-      "content"=>"<p>段落一</p><p>段落二</p><p>段落三</p>",
-      "summary"=>"段落一段落二段落三",
-      "first_photo"=>{
-        "id"=>339078012,
-        "image_url"=>"http://localhost:3000/uploads/photos/image/339078012/tony.jpg",
-        "thumb"=>"http://localhost:3000/uploads/photos/image/339078012/thumb_tony.jpg"
-      }
-    }
+    expected = {"id"=>339078012, "title"=>"大兜的測試文章", "content"=>"<p>段落一</p><p>段落二</p><p>段落三</p>", "summary"=>"段落一段落二段落三", "first_photo"=>{"id"=>339078012, "image_url"=>"http://localhost:3000/uploads/photos/image/339078012/tony.jpg", "thumb"=>"http://localhost:3000/uploads/photos/image/339078012/thumb_tony.jpg", "ziltaggings"=>[{"id"=>339078012, "x"=>20, "y"=>20, "post_id"=>339078012}]}}
     assert_equal expected, json
   end
 
@@ -28,17 +18,7 @@ class PostsControllerTest < ActionController::TestCase
     assert_response :success
     json = JSON.parse response.body
     json.first.delete 'created_on'
-    expected = {
-      "id"=>339078012,
-      "title"=>"大兜的測試文章",
-      "content"=>"<p>段落一</p><p>段落二</p><p>段落三</p>",
-      "summary"=>"段落一段落二段落三",
-      "first_photo"=>{
-        "id"=>339078012,
-        "image_url"=>"http://localhost:3000/uploads/photos/image/339078012/tony.jpg",
-        "thumb"=>"http://localhost:3000/uploads/photos/image/339078012/thumb_tony.jpg"
-      }
-    }
+    expected = {"id"=>339078012, "title"=>"大兜的測試文章", "content"=>"<p>段落一</p><p>段落二</p><p>段落三</p>", "summary"=>"段落一段落二段落三", "first_photo"=>{"id"=>339078012, "image_url"=>"http://localhost:3000/uploads/photos/image/339078012/tony.jpg", "thumb"=>"http://localhost:3000/uploads/photos/image/339078012/thumb_tony.jpg", "ziltaggings"=>[{"id"=>339078012, "x"=>20, "y"=>20, "post_id"=>339078012}]}}
     assert_equal expected, json.first
   end
 
@@ -56,19 +36,7 @@ class PostsControllerTest < ActionController::TestCase
     json.delete 'id'
     json['ziltagging'].delete 'id'
     json.delete 'created_on'
-    expected = {"title"=>"謝謝你",
-      "content"=>"<p>九五二七</p>",
-      "summary"=>"九五二七",
-      "first_photo"=>{
-        "id"=>339078012,
-        "image_url"=>"http://localhost:3000/uploads/photos/image/339078012/tony.jpg",
-        "thumb"=>"http://localhost:3000/uploads/photos/image/339078012/thumb_tony.jpg"
-      },
-      "ziltagging"=>{
-        "x"=>123, "y"=>321,
-        "photo_id"=>339078012
-      }
-    }
+    expected = {"title"=>"謝謝你", "content"=>"<p>九五二七</p>", "summary"=>"九五二七", "first_photo"=>{"id"=>339078012, "image_url"=>"http://localhost:3000/uploads/photos/image/339078012/tony.jpg", "thumb"=>"http://localhost:3000/uploads/photos/image/339078012/thumb_tony.jpg", "ziltaggings"=>[{"id"=>339078012, "x"=>20, "y"=>20, "post_id"=>339078012}, {"id"=>339078013, "x"=>123, "y"=>321, "post_id"=>339078014}]}, "ziltagging"=>{"x"=>123, "y"=>321, "photo_id"=>339078012}}
     assert_equal expected, json
   end
 
@@ -108,7 +76,7 @@ class PostsControllerTest < ActionController::TestCase
     assert_equal '<p>世界</p>', post.content
     json = JSON.parse response.body
     json.delete 'created_on'
-    expected = {"id"=>339078012, "title"=>"哈囉", "content"=>"<p>世界</p>", "summary"=>"世界", "first_photo"=>{"id"=>339078012, "image_url"=>"http://localhost:3000/uploads/photos/image/339078012/tony.jpg", "thumb"=>"http://localhost:3000/uploads/photos/image/339078012/thumb_tony.jpg"}}
+    expected = {"id"=>339078012, "title"=>"哈囉", "content"=>"<p>世界</p>", "summary"=>"世界", "first_photo"=>{"id"=>339078012, "image_url"=>"http://localhost:3000/uploads/photos/image/339078012/tony.jpg", "thumb"=>"http://localhost:3000/uploads/photos/image/339078012/thumb_tony.jpg", "ziltaggings"=>[{"id"=>339078012, "x"=>20, "y"=>20, "post_id"=>339078012}]}}
     assert_equal expected, json
   end
 
