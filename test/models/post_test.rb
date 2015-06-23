574d76c279505e28baa6bdc38d6e311e70fb6347
Jian Weihang <tonytonyjan@gmail.com>
2015-06-23 22:44:03 +0800

    在 API 加上 tag_list [#153]

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -6,12 +6,12 @@ class PostTest < ActiveSupport::TestCase
   end
 
   test "tag_list" do
-    assert_equal 'Rails,Ruby', posts(:tony).tag_list
+    assert_equal %w[Rails Ruby], posts(:tony).tag_list.split(',').sort!
     posts(:tony).tag_list = 'Rails'
     assert_equal 'Rails', posts(:tony).tag_list
     posts(:tony).tag_list = 'Ruby'
     assert_equal 'Ruby', posts(:tony).tag_list
     posts(:tony).tag_list = 'AAA,BBB,CCC'
-    assert_equal 'AAA,BBB,CCC', posts(:tony).tag_list
+    assert_equal %w[AAA BBB CCC], posts(:tony).tag_list.split(',').sort!
   end
 end
