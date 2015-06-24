3dce2d5222b4eb31c62e236e93de7285e943c827
Jian Weihang <tonytonyjan@gmail.com>
2015-06-24 23:03:28 +0800

    整合發佈功能 [#150]

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   layout 'sidebar'.freeze
 
   def home
-    @ziltaggings = Ziltagging.includes(:photo, post: :user).order('id DESC').page(params[:page]).per(10)
+    @ziltaggings = Ziltagging.includes(:photo, post: :user).where(posts: {published: true}).order('ziltaggings.id DESC').page(params[:page]).per(10)
     if params[:scroll]
       render :partial => "/partials/post_article", collection: @ziltaggings, as: :ziltagging
     end
