55cfc7ee137976c745c9250c1441929d978f4ef1
Carlos Chang <chang.abin@gmail.com>
2015-06-26 10:09:07 +0800

    收藏頁 [#155]

--- app/assets/javascripts/application/users/collecting.js
+++ app/assets/javascripts/application/users/collecting.js
@@ -1,15 +1,31 @@
 // profile 收藏頁的輪播
 
 main(function(){
-  
+
   if( $("body").is(".users.collecting") ){
     $('div.show_slick').slick({
       infinite: false,
-      slidesToShow: 8,
-      slidesToScroll: 8
+      slidesToShow: 16,
+      slidesToScroll: 16
+    });
+
+    $("a.square_image").on("click", function(e){
+      e.preventDefault();
+
+      var source   = $("#handlebar_ziltag_thumb_show_one").html();
+      var template = Handlebars.compile(source);
+      var context = {
+        image_url: $(this).data("image-url"),
+        x: $(this).data("x"),
+        y: $(this).data("y"),
+        link: $(this).data("link")
+      };
+      var html    = template(context);
+      $(this).closest(".post_article").find("article.c_ziltag_sticker_article").html(html);
+
     });
   }
-  
+
 });
 
 
