e7143e0883866aa72fa16f9d7f33369b8f9f5da0
Jian Weihang <tonytonyjan@gmail.com>
2015-07-02 13:26:25 +0800

    bin/rake db:migrate [#154]

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150623073748) do
+ActiveRecord::Schema.define(version: 20150624015102) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -73,6 +74,26 @@ ActiveRecord::Schema.define(version: 20150623073748) do
 
   add_index "posts", ["user_id"], name: "index_posts_on_user_id", using: :btree
 
+  create_table "taggings", force: :cascade do |t|
+    t.integer  "tag_id"
+    t.integer  "taggable_id"
+    t.string   "taggable_type"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
+
+  add_index "taggings", ["tag_id", "taggable_id", "taggable_type"], name: "index_taggings_on_tag_id_and_taggable_id_and_taggable_type", unique: true, using: :btree
+  add_index "taggings", ["tag_id"], name: "index_taggings_on_tag_id", using: :btree
+  add_index "taggings", ["taggable_type", "taggable_id"], name: "index_taggings_on_taggable_type_and_taggable_id", using: :btree
+
+  create_table "tags", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "tags", ["name"], name: "index_tags_on_name", using: :btree
+
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
@@ -119,6 +140,7 @@ ActiveRecord::Schema.define(version: 20150623073748) do
   add_foreign_key "followings", "users", column: "leader_id"
   add_foreign_key "photos", "users"
   add_foreign_key "posts", "users"
+  add_foreign_key "taggings", "tags"
   add_foreign_key "ziltaggings", "photos"
   add_foreign_key "ziltaggings", "posts"
 end
