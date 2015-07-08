0ba0752824e3d2d108d06cdae55a846599ff1d21
Jian Weihang <tonytonyjan@gmail.com>
2015-07-08 12:04:33 +0800

    註冊、登入頁圖片

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,4 +12,10 @@ module ApplicationHelper
     end
   end
 
+  def feature_ziltag
+    if photo = Photo.find_by(id: Settings[:feature_photo_id])
+      ziltag_wrapper photo, photo.ziltaggings
+    end
+  end
+
 end
