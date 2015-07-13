ab84079754c83d2364132b92762cdb04dd01ea32
Carlos Chang <chang.abin@gmail.com>
2015-07-13 16:16:56 +0800

    留言位置 [#171]

--- db/schema.rb
+++ db/schema.rb
@@ -76,6 +76,12 @@ ActiveRecord::Schema.define(version: 20150710063357) do
 
   add_index "posts", ["user_id"], name: "index_posts_on_user_id", using: :btree
 
+  create_table "students", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "taggings", force: :cascade do |t|
     t.integer  "tag_id"
     t.integer  "taggable_id"
