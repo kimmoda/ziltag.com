78e1c1f2d109e92594250a5d2fcbba7642c01418
Jian Weihang <tonytonyjan@gmail.com>
2015-07-14 13:55:38 +0800

    列出熱門標籤 [#173]

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,5 +1,7 @@
 class Tag < ActiveRecord::Base
   # scopes
+  # TODO: use counter cache
+  scope :top_5, ->{ Tag.joins(:posts).select('tags.*, count(posts.id) AS posts_count').group(:id).order("posts_count DESC").limit(5) }
 
   # constants
 
