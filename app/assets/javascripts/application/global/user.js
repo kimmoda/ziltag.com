374e480cfe91c2e4e0d28c6b7ee4364e1c3883bb
Carlos Chang <chang.abin@gmail.com>
2015-07-15 15:51:46 +0800

    顯示圖片的第一層留言，先固定抓取圖片id為1的留言 [#171]

---
+++ app/assets/javascripts/application/global/user.js
@@ -0,0 +1,13 @@
+// 記錄使用者的資訊
+var user = {
+  id: null,
+  login_status: false
+};
+
+main(function(){
+
+  if( $("li[data-user-id]").data("user-id") != undefined ){
+    user.id = $("li[data-user-id]").data("user-id");
+    user.login_status = true;
+  }
+});
