e4e32c23d490896c6f8eb6f6723b6dc228659636
Carlos Chang <chang.abin@gmail.com>
2015-07-16 12:10:17 +0800

    會員新增子留言 [#171]

--- app/assets/javascripts/application/global/user.js
+++ app/assets/javascripts/application/global/user.js
@@ -8,6 +8,7 @@ main(function(){
 
   if( $("li[data-user-id]").data("user-id") != undefined ){
     user.id = $("li[data-user-id]").data("user-id");
+    user.username = $("li[data-user-id]").data("user-username");
     user.login_status = true;
   }
 });
