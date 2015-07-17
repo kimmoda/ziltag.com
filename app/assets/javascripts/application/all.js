735969fc8a631e5d95db59e7573b575eab99ba4d
Carlos Chang <chang.abin@gmail.com>
2015-07-17 15:53:17 +0800

    可刪除自己的子留言 [#171]

--- app/assets/javascripts/application/all.js
+++ app/assets/javascripts/application/all.js
@@ -1,19 +1,19 @@
 var allJs = {
-  
+
   // 調整layout的介面
   tune_layout: function(){
-    
+
     // 針對 div[data-layout=outer_fill_white] 的留白區
     if( $(window).width() < 768 ){
       var left_side_width = 0;
     }else{
       var left_side_width = ($(window).width() - $("div[data-layout=inner]").width()) / 2;
     }
-    
+
     // 針對 div[data-layout=custom_header_line] 分隔線
     var col_right = $("div[data-layout=col_right]").outerWidth();
     $("div[data-layout=custom_header_line]").css("width", (col_right + left_side_width + 1) + "px");
-    
+
     // 調整 aside.aside 的高度及寬度
     $("aside[data-layout=aside]").css("height", ($(window).height() - 60) + "px");
     if( $(window).width() < 768 ){
@@ -27,25 +27,35 @@ var allJs = {
         "width": ( left_side_width + $("div[data-layout=col_logo]").width() ) + "px"
       });
     }
-    
+
     $("[data-layout=full_interface]").css({
         "max-width": ($("aside[data-layout=aside]").outerWidth() + col_right) + "px",
         "padding-left": $("aside[data-layout=aside]").outerWidth() + "px"
     });
   },
-  
+
   // 選擇圖片預覽圖
   readURL: function(selectedFile) {
     if (selectedFile.files && selectedFile.files[0]) {
       var reader = new FileReader();
       reader.onload = function (e) {
-        
+
         $("button[data-trigger-select-image]").addClass("hidden");
         $("div[data-profile-img-block]").removeClass("hidden");
-        
+
         $('img[' + $(selectedFile).data("target") + ']').attr('src', e.target.result);
       }
       reader.readAsDataURL(selectedFile.files[0]);
     }
+  },
+
+  // handlebar 條件式判斷 2 個值是否相同
+  handlebar_register_ifCond: function(){
+    Handlebars.registerHelper('ifCond', function(v1, v2, options) {
+      if(v1 === v2) {
+        return options.fn(this);
+      }
+      return options.inverse(this);
+    });
   }
 };
