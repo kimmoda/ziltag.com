f2f25e0060f5a0f5e07bea69ae877466009cff4a
Carlos Chang <chang.abin@gmail.com>
2015-07-17 18:12:13 +0800

    樂貼與留言的切換 [#171]

--- app/assets/javascripts/application/components/tag_or_msg.js
+++ app/assets/javascripts/application/components/tag_or_msg.js
@@ -3,11 +3,21 @@
 main(function(){
   if( $("[data-component=tag_or_msg]").length > 0 ){
     $("[data-component=tag_or_msg]").find("button").each(function(){
-      
+
       $(this).on("click", function(){
         $(this).closest("[data-component=tag_or_msg]").find("button").toggleClass("active");
+        $(this).closest("article.post_ziltag_article").find("div.get_comments_block").toggleClass("hidden");
+
+        $(this).closest("article.post_ziltag_article").find("div.ziltag_wrapper").find("a").toggleClass("hidden");
+        $(this).closest("article.post_ziltag_article").find("div.parent_comment").toggleClass("hidden");
+
+        if( $(this).hasClass("show_ziltag") ){
+
+        }
+        if( $(this).hasClass("show_comments") ){
+        }
       });
-      
+
     });
   }
 });
--- app/assets/javascripts/application/components/ziltag_modal.js
+++ app/assets/javascripts/application/components/ziltag_modal.js
@@ -21,41 +21,40 @@ function back_page(){
 }
 
 main(function(){
-  
+
   ziltag_modal.modal = document.getElementById('ziltag_modal');
   ziltag_modal.section_post = document.querySelector('[data-layout="col_right"]');
-  
-  
+
+
   $('a[data-btn-remove]').attr("data-back-path", location.pathname + location.search);
-  
+
   $('a[data-btn-remove]').on('click', function(e){
     e.preventDefault();
     window.history.back();
   });
-  
+
   // TODO: 重複點擊相同的文章，會重複發送請求索取 JSON 資料，可以在第一次發送時就 cache 結果，減輕伺服器負擔。
   $(document).on('ajax:success', '[data-ziltag-modal]', function(event, data){
-    
+
     $("[data-modal-post-ziltag-article]").attr("data-modal-status", "open");
-    
+
     // 置換內容
     ZiltagSticker.replaceModalData(data, null);
-    
+
     ZiltagSticker.appendImgOnModal(data);
-    
+
     showPostModal();
-    
   });
-  
+
 });
 
 
 $(window).resize(function(){
-  
+
   if( $("[data-modal-post-ziltag-article]").attr("data-modal-status") == "open" ){
     showPostModal();
   }
-  
+
 });
 
 window.addEventListener("popstate", function(e) {
