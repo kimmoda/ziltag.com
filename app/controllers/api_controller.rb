d8ed6a2e24588048e1d7ce8d437236410a8fa252
Jian Weihang <tonytonyjan@gmail.com>
2015-08-07 15:41:59 +0800

    corss domain only in API namespace

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,4 +1,13 @@
 class ApiController < ApplicationController
   skip_before_action :verify_authenticity_token
+  before_action :set_headers
   before_action :authenticate_user!, only: %i[create update destroy]
+
+private
+
+  def set_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Credentials'] = 'true'
+  end
+
 end
--- config/application.rb
+++ config/application.rb
@@ -37,10 +37,5 @@ module Ziltag
       Devise::PasswordsController.layout 'normal'.freeze
     end
 
-    config.action_dispatch.default_headers = {
-      'Access-Control-Allow-Origin' => '*',
-      'Access-Control-Allow-Credentials' => 'true'
-    }
-
   end
 end
