569e71a46894b55a97434e048bb776b1495fdf46
Jian Weihang <tonytonyjan@gmail.com>
2015-08-11 03:50:38 +0800

    refactoring collecting components

--- app/helpers/collecting_helper.rb
+++ app/helpers/collecting_helper.rb
@@ -1,32 +1,18 @@
+=begin
+<div class="collecting-button" data-id="ID" data-type="TYPE">
+  <i class="fa fa-heart collecting-button__collect"></i>
+  <i class="fa fa-heart collecting-button__uncollect"></i>
+</div>
+=end
+
 module CollectingHelper
-  def collecting_link record, dom_class
-    case dom_class
-    when :collect
-      link_method = :post
-      link_style = current_user.collect?(record) ? :'display:none' : nil
-    when :uncollect then :delete
-      link_method = :delete
-      link_style = current_user.collect?(record) ? nil : :'display:none'
-    else raise "Unknown dom_class: #{dom_class}"
-    end
-
-    link_options = {id: record.id, type: record.class.name}
-    link_path = dom_class == :collect ? collect_path(link_options) : uncollect_path(link_options)
-    link_to(fa(:star), link_path,
-      data: {class: dom_id(record)},
-      class: "#{dom_class} #{dom_id(record)}",
-      method: link_method,
-      remote: true,
-      style: link_style
-    )
-  end
-
   def collecting_button record
     return unless user_signed_in?
     return if record.try(:user) == current_user
-    ret = ActiveSupport::SafeBuffer.new
-    ret << collecting_link(record, :collect)
-    ret << collecting_link(record, :uncollect)
+    content_tag :div, class: 'collecting-button', data: {id: record.id, type: record.class.name} do
+      concat content_tag :i, nil, class: 'fa fa-heart collecting-button__collect', style: (:'display:none' if current_user.collect? record)
+      concat content_tag :i, nil, class: 'fa fa-heart collecting-button__uncollect', style: (:'display:none' unless current_user.collect? record)
+    end
   end
 
 end
