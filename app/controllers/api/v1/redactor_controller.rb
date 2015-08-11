8cf1ed4f0d434b1f88dccc0c3b8548ba311f2051
Jian Weihang <tonytonyjan@gmail.com>
2015-08-12 01:34:27 +0800

    fix redactor api

--- app/controllers/api/v1/redactor_controller.rb
+++ app/controllers/api/v1/redactor_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::RedactorController < ApiController
   def upload
     @photo = Photo.new image: params.require(:file)
     if @photo.save
-      render template: 'api/v1/photos/show'
+      render json: {filelink: @photo.image_url}
     else
       render json: @photo.errors, status: :unprocessable_entity
     end
