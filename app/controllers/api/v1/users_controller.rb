7790058abcc4955bfc66fdf64a41b04353ee4d1b
Jian Weihang <tonytonyjan@gmail.com>
2015-08-12 13:58:40 +0800

    PUT /api/v1/users/:id

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -5,13 +5,21 @@ class Api::V1::UsersController < ApiController
   end
 
   def update
-    # TODO
+    if @user == current_user && @user.update(user_params)
+      render :show
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
   end
 
 private
 
   def set_user
     @user = params[:id] == 'me' ? current_user : User.find_by!(username: params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:avatar, :cover)
   end
 
 end
