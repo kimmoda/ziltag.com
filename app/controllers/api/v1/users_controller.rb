e96b6d289f5a6791759e73b580976a9360dc1855
Jian Weihang <tonytonyjan@gmail.com>
2015-08-12 13:29:38 +0800

    fix params

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -11,7 +11,7 @@ class Api::V1::UsersController < ApiController
 private
 
   def set_user
-    @user = params[:id] == 'me' ? current_user : User.find(params[:id])
+    @user = params[:id] == 'me' ? current_user : User.find_by!(username: params[:id])
   end
 
 end
