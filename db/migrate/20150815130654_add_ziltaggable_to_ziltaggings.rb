c770fd910e5cea634d182c4aa557713532606677
Jian Weihang <tonytonyjan@gmail.com>
2015-08-15 21:55:31 +0800

    migrate [#208]

---
+++ db/migrate/20150815130654_add_ziltaggable_to_ziltaggings.rb
@@ -0,0 +1,22 @@
+class AddZiltaggableToZiltaggings < ActiveRecord::Migration
+  def change
+    add_reference :ziltaggings, :ziltaggable, polymorphic: true, index: true
+
+    reversible do |dir|
+      Ziltagging.find_each do |ziltagging|
+        dir.up{ ziltagging.update_columns ziltaggable_id: ziltagging.post_id, ziltaggable_type: :Post }
+        dir.down{ ziltagging.update_columns post_id:  ziltagging.ziltaggable_id }
+      end
+      dir.up do
+        change_column_null :ziltaggings, :ziltaggable_id, false
+        change_column_null :ziltaggings, :ziltaggable_type, false
+      end
+      dir.down do
+        change_column_null :ziltaggings, :post_id, false
+        add_foreign_key :ziltaggings, :posts
+      end
+    end
+
+    remove_reference :ziltaggings, :post, index: true
+  end
+end
