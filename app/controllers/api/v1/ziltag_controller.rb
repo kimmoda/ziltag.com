7c9a08dc2be76cc07275ba2f8cb56ac42c63c226
Jian Weihang <tonytonyjan@gmail.com>
2015-08-15 23:38:23 +0800

    refactor controller and abstract models. [#208]

--- app/controllers/api/v1/ziltag_controller.rb
+++ app/controllers/api/v1/ziltag_controller.rb
@@ -1,23 +1,50 @@
 class Api::V1::ZiltagController < ApiController
+  ALLOWED_TAGGABLE_TYPES = %w[post]
+
   before_action :authenticate_user!, only: :ziltag
+  before_action :set_ziltaggable_type!, only: :ziltag
+
 
   # POST /api/v1/ziltag
   def ziltag
     @ziltag = Abstract::Ziltag.new({
       photo: Photo.find_or_create_by_url!(photo_params),
-      post: current_user.posts.new(post_params),
+      ziltaggable: ziltaggable_new,
       ziltagging: Ziltagging.new(ziltagging_params)
     })
     @ziltag.save!
   rescue
-    render json: {
-      photo: @ziltag.photo.errors,
-      post: @ziltag.post.errors,
-      ziltagging: @ziltag.ziltagging.errors
-    }, status: :unprocessable_entity
+    if @ziltag
+      render json: {
+        photo: @ziltag.photo.errors,
+        ziltaggable: @ziltag.ziltaggable.errors,
+        ziltagging: @ziltag.ziltagging.errors
+      }, status: :unprocessable_entity
+    else
+      render json: {error: $@}
+    end
   end
 
 private
+
+  def set_ziltaggable_type!
+    params.keys.each do |key|
+      if ALLOWED_TAGGABLE_TYPES.include? key
+        @ziltaggable_type = key
+        break
+      end
+    end
+  end
+
+  def ziltaggable_new
+    ziltaggable = @ziltaggable_type.classify.constantize.new ziltaggable_params
+    ziltaggable.user = current_user
+    ziltaggable
+  end
+
+  def ziltaggable_params
+    send "#{@ziltaggable_type}_params"
+  end
 
   def photo_params
     params.require(:photo).permit(:image, :remote_image_url)
