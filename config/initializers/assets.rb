2dec572d5acf13bae45dc41641d1ae7e14d79487
Jian Weihang <tonytonyjan@gmail.com>
2015-08-21 17:11:43 +0800

    first page of landing page

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -4,10 +4,17 @@
 Rails.application.config.assets.version = '1.0'
 
 # Add additional assets to the asset load path
-# Rails.application.config.assets.paths << Emoji.images_path
+Rails.application.config.assets.paths.concat [
+  'vendor/assets/components',
+].concat Dir['vendor/assets/components/**/*/fonts']
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
-Rails.application.config.assets.precompile += %w[admin.css admin.js embedded.css embedded.js embedded/ziltagging.css embedded/ziltagging.js]
+Rails.application.config.assets.precompile += %w[
+  admin.css admin.js
+  embedded.css embedded.js
+  embedded/ziltagging.css embedded/ziltagging.js
+  landing.css landing.js
+]
 Rails.application.config.assets.precompile << /\.(?:svg|eot|woff|woff2|ttf|gif)\z/
