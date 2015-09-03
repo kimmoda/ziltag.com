a8b39f2c00fd7876c3dc979e5f8cc1da34bb7344
Jian Weihang <tonytonyjan@gmail.com>
2015-09-04 05:29:45 +0800

    update comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,60 +1,22 @@
 class Comment < ActiveRecord::Base
   # scopes
-  scope :where_image_url_is, ->(url){ joins(:photo).where(photos: {source: url}) }
-  scope :is_root, ->{ where(root: nil) }
 
   # constants
 
   # attributes
-  delegate :image_url, to: :photo
 
   # associations
   belongs_to :user
-  belongs_to :photo
-  belongs_to :root, class_name: Comment, foreign_key: :comment_id
-  has_many :children, ->{ order(:created_at) }, class_name: Comment, dependent: :destroy
-  has_many :latest_children, ->{ order(:created_at).limit(10) }, class_name: Comment
+  belongs_to :sticker
 
   # validations
-  validates :photo, :text, presence: true
-  validate :identity # either email or user exists
-  validate :coordinates # either coordinates or comment_id should exists
+  validates :photo, :content, presence: true
 
   # callbacks
-  before_validation :set_email_from_user
-  before_validation :set_coordinates_from_root, if: :child?
 
   # other
   def to_s
-    text
-  end
-
-  def child?
-    root.present?
-  end
-
-private
-
-  def identity
-    unless email.present? || user.present?
-      errors.add :base, 'either email or user should exists.'
-    end
-  end
-
-  def coordinates
-    unless root.present? || (x.present? && y.present?)
-      errors.add :bases, 'either coordinates or comment_id should exists.'
-    end
-  end
-
-  def set_email_from_user
-    self.email = user.email if user.present?
-  end
-
-  def set_coordinates_from_root
-    if root.present?
-      self.x, self.y = root.x, root.y
-    end
+    content
   end
 
 end
