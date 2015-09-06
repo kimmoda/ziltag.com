98eb0c0f237ca14b683339dde540f54664976318
Jian Weihang <tonytonyjan@gmail.com>
2015-09-07 04:02:46 +0800

    finish login/sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,8 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
+
   # scopes
-  scope :hot_users, ->{ all }
 
   # constants
 
