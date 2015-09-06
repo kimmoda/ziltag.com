cedb1bfd5cada88622168e329b173e4212d7167c
Jian Weihang <tonytonyjan@gmail.com>
2015-09-07 04:41:48 +0800

    set redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -58,4 +58,14 @@ protected
     end
   end
 
+private
+
+  def after_sign_in_path_for(resource_or_scope)
+    request.referer || root_path
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+    request.referer || root_path
+  end
+
 end
