f55e148853f99442df5bac9f13a26b1226b60981
Jian Weihang <tonytonyjan@gmail.com>
2015-09-15 02:36:37 +0800

    remove useless helpers

--- app/helpers/font_awesome_helper.rb
+++
@@ -1,8 +0,0 @@
-module FontAwesomeHelper
-  # <i class="fa fa-picture-o"></i> 以圖找貼
-  def fa name, text = nil
-    ret = content_tag(:i, nil, class: "fa fa-#{name}")
-    ret << ' ' << text if text
-    ret
-  end
-end
--- app/helpers/posts_helper.rb
+++
@@ -1,8 +0,0 @@
-module PostsHelper
-  # TODO: 寫 test
-  def summary content, length: 300, **options
-    content = strip_tags(content)
-    length -= content[0, length].scan(/\p{Han}/).size/2 - 1
-    truncate content, {length: length}.merge!(options)
-  end
-end
