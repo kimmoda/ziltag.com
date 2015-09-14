a88bd9829d31ef0866576def533d8631c997e8b0
Jian Weihang <tonytonyjan@gmail.com>
2015-09-15 02:31:18 +0800

    rename helpers

--- app/helpers/ziltags_helper.rb
+++ app/helpers/ziltags_helper.rb
@@ -1,13 +1,13 @@
 require 'content_retriever'
-module StickersHelper
-  def sticker_preview sticker
+module ZiltagsHelper
+  def ziltag_preview sticker
     c = ContentRetriever.new(sticker.content)
     if c.recognizer
       render partial: c.recognizer.class.name.underscore, object: c.recognizer
     end
   end
 
-  def sticker_content sticker
+  def ziltag_content sticker
     sanitize sticker.content.gsub URI.regexp(['http', 'https']), '<a href="\0" target="_blank">\1://\4/...</a>'
   end
 end
