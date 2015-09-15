7dd463f793bd1cf6596ddf1c7247947630d39516
Jian Weihang <tonytonyjan@gmail.com>
2015-09-15 16:09:15 +0800

    update API format

--- app/controllers/api/v1/ziltags_controller.rb
+++ app/controllers/api/v1/ziltags_controller.rb
@@ -1,12 +1,7 @@
 class Api::V1::ZiltagsController < ApiController
   def index
-    if params[:src]
-      @photo = Photo.find_by source: params[:src]
-      @photo ||= Photo.create remote_image_url: params[:src]
-      @ziltags = @photo.ziltags.includes(:user)
-      headers['X-Map'] = @photo.slug
-    else
-      @ziltags = Ziltag.all.page(params[:page])
-    end
+    @photo = Photo.find_by source: params[:src]
+    @photo ||= Photo.create remote_image_url: params[:src]
+    @ziltags = @photo.ziltags.includes(:user)
   end
 end
--- app/views/api/v1/ziltags/index.json.jbuilder
+++ app/views/api/v1/ziltags/index.json.jbuilder
@@ -1,7 +1,9 @@
-json.array!(@ziltags) do |ziltag|
-  json.id ziltag.slug
-  json.map ziltag.photo.slug
-  json.extract! ziltag, :x, :y
-  json.usr ziltag.username
-  json.preview truncate ziltag.content
+json.map @photo.slug
+json.ziltags do
+  json.array!(@ziltags) do |ziltag|
+    json.id ziltag.slug
+    json.extract! ziltag, :x, :y
+    json.usr ziltag.username
+    json.preview truncate ziltag.content
+  end
 end
