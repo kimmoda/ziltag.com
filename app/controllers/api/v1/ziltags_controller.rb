e829868dae78a54c7eb46a2153833e83dfbe8d80
Jian Weihang <tonytonyjan@gmail.com>
2015-09-28 21:21:39 +0800

    filter unconfirmed ziltags

--- app/controllers/api/v1/ziltags_controller.rb
+++ app/controllers/api/v1/ziltags_controller.rb
@@ -2,6 +2,6 @@ class Api::V1::ZiltagsController < ApiController
   def index
     @photo = Photo.find_by source: params[:src]
     @photo ||= Photo.create remote_image_url: params[:src]
-    @ziltags = @photo.ziltags.includes(:user)
+    @ziltags = @photo.ziltags.includes(:user).select(&:confirmed?)
   end
 end
