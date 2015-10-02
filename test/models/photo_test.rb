1e49943fa3aeb1750f52e16581e901586d427f1f
Jian Weihang <tonytonyjan@gmail.com>
2015-10-02 21:28:07 +0800

    update test

--- test/models/photo_test.rb
+++ test/models/photo_test.rb
@@ -7,10 +7,12 @@ class PhotoTest < ActiveSupport::TestCase
     assert photo.persisted?
   end
 
-  def test_duplicate_source
+  def test_duplicated_source
     stub_request_for_image
     p1 = Photo.find_or_create_by_source_and_href! 'http://webmock.me/jpeg', 'http://webmock.me'
-    p2 = Photo.find_or_create_by_source_and_href! 'http://webmock.me/jpeg'
-    refute_equal p1, p2
+    p2 = Photo.find_or_create_by_source_and_href! 'http://webmock.me/jpeg', 'http://webmock.me'
+    p3 = Photo.find_or_create_by_source_and_href! 'http://webmock.me/jpeg'
+    assert_equal p1, p2
+    refute_equal p1, p3
   end
 end
