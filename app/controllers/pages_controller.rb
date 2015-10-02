35c5ad8c7bc5ad4368cd3c9e611780e378bb6343
Jian Weihang <tonytonyjan@gmail.com>
2015-10-03 03:48:22 +0800

    setup page access

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,6 @@
 class PagesController < ApplicationController
+  before_action :must_sign_in!, :must_be_content_provider!, only: %i[username update_username install]
+
   def home
     if params[:src]
       @photo = Photo.find_or_create_by_source_and_href! params[:src]
@@ -9,6 +11,10 @@ class PagesController < ApplicationController
     end
   end
 
+  def username
+    redirect_to install_path if current_user.username.present?
+  end
+
   def update_username
     if current_user.update params.require(:content_provider).permit(:username)
       redirect_to install_path, notice: 'updated'
@@ -17,4 +23,15 @@ class PagesController < ApplicationController
       render :username
     end
   end
+
+private
+
+  def must_sign_in!
+    redirect_to root_path, alert: 'You are not signed in.' unless user_signed_in?
+  end
+
+  def must_be_content_provider!
+    redirect_to root_path, alert: 'You are not a content provider.' unless current_user.is_a? ContentProvider
+  end
+
 end
