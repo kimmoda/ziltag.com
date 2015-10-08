3186de8b4631f8b62ba3714eb99ccc97ac931dff
Jian Weihang <tonytonyjan@gmail.com>
2015-10-08 17:41:24 +0800

    fix nil issue

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,7 +3,7 @@ class PhotosController < ApplicationController
     @photo = Photo.find_by!(slug: params[:slug])
     @ziltags = @photo.ziltags.confirmed(current_user)
     @ziltag = @photo.ziltags.find_by(slug: params[:ziltag_slug])
-    @comments = @ziltag.comments.includes(:user).confirmed(current_user)
+    @comments = @ziltag.comments.includes(:user).confirmed(current_user) if @ziltag
     @user = User.new unless user_signed_in?
     session[:previous_photo_path] = request.fullpath
   end
