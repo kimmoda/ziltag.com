d3e3c2a00bb706d364a3499728b265a0d8034a43
Jian Weihang <tonytonyjan@gmail.com>
2015-10-28 19:10:11 +0800

    fix migration

--- db/migrate/20151028083550_add_host_and_path_to_photos.rb
+++ db/migrate/20151028083550_add_host_and_path_to_photos.rb
@@ -5,7 +5,7 @@ class AddHostAndPathToPhotos < ActiveRecord::Migration
     add_column :photos, :path, :string
     add_index :photos, :path
     Photo.where('href IS NOT NULL').find_each do |photo|
-      photo.update! href: photo.href
+      photo.update! href: photo.href if photo.href?
     end
   end
 end
