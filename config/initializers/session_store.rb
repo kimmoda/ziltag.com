a119b7b906473ce8cc182e27cef99bc18e22b8b9
Jian Weihang <tonytonyjan@gmail.com>
2015-11-12 01:44:54 +0800

    fix session

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_ziltag_session', domain: :all, tld_length: 2
+if Rails.env.production?
+  Rails.application.config.session_store :cookie_store, key: '_ziltag_session', domain: ".#{Settings.host}"
+else
+  Rails.application.config.session_store :cookie_store, key: '_ziltag_session'
+end
