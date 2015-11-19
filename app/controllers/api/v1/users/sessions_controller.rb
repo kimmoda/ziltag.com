21496f816c938d58a8b0c4c645108938c5c82598
Jian Weihang <tonytonyjan@gmail.com>
2015-11-19 16:08:54 +0800

    fix devise cros

--- app/controllers/api/v1/users/sessions_controller.rb
+++ app/controllers/api/v1/users/sessions_controller.rb
@@ -1,4 +1,11 @@
 class Api::V1::Users::SessionsController < Devise::SessionsController
   skip_before_action :verify_authenticity_token
+  before_action :set_headers
   respond_to :json
+
+private
+
+  def set_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+  end
 end
--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,7 +1,8 @@
 class RegistrationsController < Devise::RegistrationsController
   skip_before_action :verify_authenticity_token
+  before_action :set_headers
 
-  protected
+protected
 
   def update_resource(resource, params)
     resource.update_without_password(params)
@@ -9,6 +10,12 @@ class RegistrationsController < Devise::RegistrationsController
 
   def after_update_path_for(resource)
     edit_registration_path(resource)
+  end
+
+private
+
+  def set_headers
+    headers['Access-Control-Allow-Origin'] = '*'
   end
 
 end
