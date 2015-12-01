eb8107b1c1f1921582b4c7076de7c66a9b78714b
Jian Weihang <tonytonyjan@gmail.com>
2015-12-01 16:24:37 +0800

    fix migration

--- db/migrate/20151201074814_add_geo_to_photos.rb
+++ db/migrate/20151201074814_add_geo_to_photos.rb
@@ -3,9 +3,13 @@ class AddGeoToPhotos < ActiveRecord::Migration
     add_column :photos, :width, :integer
     add_column :photos, :height, :integer
     Photo.find_each do |photo|
-       photo.image.cache_stored_file!
-       width, height = ::MiniMagick::Image.open(photo.image.file.file)[:dimensions]
-       photo.update_columns width: width, height: height
+      begin
+        photo.image.cache_stored_file!
+        width, height = ::MiniMagick::Image.open(photo.image.file.file)[:dimensions]
+        photo.update_columns width: width, height: height
+      rescue
+        Rails.logger.warn "[migration] broken photo: #{photo.id}"
+      end
     end
     change_column_null :photos, :width, false
     change_column_null :photos, :height, false
