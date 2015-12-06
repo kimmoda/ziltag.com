b288d487542999fd68f3b8484e29dbca7ef0c9c9
Jian Weihang <tonytonyjan@gmail.com>
2015-12-06 16:56:58 +0800

    return 200 status code when login fail

--- app/controllers/api/v1/users/sessions_controller.rb
+++ app/controllers/api/v1/users/sessions_controller.rb
@@ -3,6 +3,15 @@ class Api::V1::Users::SessionsController < Devise::SessionsController
   before_action :set_headers
   respond_to :json
 
+  def create
+    @user = User.find_for_database_authentication(login: params[:user][:login])
+    if @user && @user.valid_password?(params[:user][:password])
+      render
+    else
+      render json: {error: 'invalid login name or password'}
+    end
+  end
+
 private
 
   def set_headers
