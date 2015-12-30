570b43c520df448598fba75b65a64cb548bc53de
Jian Weihang <tonytonyjan@gmail.com>
2015-12-31 01:58:12 +0800

    fix carrierwave configuration

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,6 +1,7 @@
 CarrierWave.configure do |config|
   if Rails.env.production?
     Settings.carrierwave.each do |key, value|
+      value = value.to_sym if key.to_s == 'storage'
       config.send("#{key}=", value.respond_to?(:symbolize_keys) ? value.symbolize_keys : value)
     end
   else
