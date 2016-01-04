c13f2310aff0385ce00bb387120cd21053ce1f79
Jian Weihang <tonytonyjan@gmail.com>
2016-01-05 04:26:08 +0800

    resolved [#265]

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -12,7 +12,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   end
 
   version :default do
-    process resize_to_fit: [510, 340]
+    process resize_to_limit: [600, nil]
     process :store_dimensions
   end
 
