644b1e7ddb19c1e4a0f94d5fc8461781825d09ff
Jian Weihang <tonytonyjan@gmail.com>
2016-01-15 19:43:41 +0800

    setup back url [#282]

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,6 +1,7 @@
 class RegistrationsController < Devise::RegistrationsController
   skip_before_action :verify_authenticity_token
   before_action :set_headers
+  before_action :set_back_url
 
 protected
 
@@ -22,6 +23,11 @@ private
 
   def set_headers
     headers['Access-Control-Allow-Origin'] = '*'
+  end
+
+  def set_back_url
+    session[:back_url] = request.referer unless request.referer =~ /\/users/
+    @back_url = session[:back_url] || session[:previous_photo_path] || root_path
   end
 
 end
