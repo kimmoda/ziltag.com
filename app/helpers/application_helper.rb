b022af3e5fe2c979337f4809749b017d46281845
Jian Weihang <tonytonyjan@gmail.com>
2016-02-18 00:01:22 +0800

    these helpers are not used

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,21 +1,2 @@
 module ApplicationHelper
-  def flash_messages
-    ret = ActiveSupport::SafeBuffer.new
-    ret << alert_message(notice, :info) if notice
-    ret << alert_message(alert, :danger) if alert
-    ret
-  end
-
-  def time_info date_or_time, opts = {}
-    time_tag date_or_time, {class: :datetime}.merge!(opts) do
-      '發表於 ' << l(date_or_time, format: :long)
-    end
-  end
-
-  def link_to_with_active_class name = nil, options = nil, html_options = nil, &block
-    html_options ||= {}
-    html_options.merge! class: 'active' if current_page?(options)
-    link_to name, options, html_options, &block
-  end
-
 end
