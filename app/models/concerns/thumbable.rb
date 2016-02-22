7af0285653b064287e3baed17f5e02b165c2c080
Jian Weihang <tonytonyjan@gmail.com>
2016-02-23 00:09:54 +0800

    double thumbnail's quality (for retina screen) [#333]

--- app/models/concerns/thumbable.rb
+++ app/models/concerns/thumbable.rb
@@ -4,22 +4,7 @@ module Thumbable
   included do
     include CarrierWave::MiniMagick
     version :thumb do
-      process resize_to_fill_space: [50, 50]
-    end
-  end
-
-  # TODO: resize_to_fill 在 graphicsmagick 上會有問題
-  # https://github.com/carrierwaveuploader/carrierwave/issues/1520
-  def resize_to_fill_space(width, height, gravity = 'Center')
-    manipulate! do |image|
-      MiniMagick::Tool::Convert.new do |convert|
-        convert << image.path # input
-        convert.thumbnail "#{width}x#{height}^"
-        convert.gravity gravity
-        convert.extent "#{width}x#{height}"
-        convert << image.path # output
-      end
-      image
+      process resize_to_fill: [100, 100]
     end
   end
 
