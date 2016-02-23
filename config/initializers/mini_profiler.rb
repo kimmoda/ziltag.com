1a233c25492f5e873d5fb59df2c9d64a46cf90ee
Jian Weihang <tonytonyjan@gmail.com>
2016-02-23 16:29:11 +0800

    do not profile test pages

--- config/initializers/mini_profiler.rb
+++ config/initializers/mini_profiler.rb
@@ -1,3 +1,3 @@
 if Rails.env.development?
-  Rack::MiniProfiler.config.skip_paths << '/uploads' << '/images' << '/favicon.png' << '/embedded'
+  Rack::MiniProfiler.config.skip_paths << '/uploads' << '/images' << '/favicon.png' << '/embedded' << '/rails'
 end
