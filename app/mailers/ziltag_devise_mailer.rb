c04458bc25a1211803cf57f3cc3ba1751ab516f0
Jian Weihang <tonytonyjan@gmail.com>
2016-02-23 21:31:36 +0800

    integrate notification email style with new layout [#336]

--- app/mailers/ziltag_devise_mailer.rb
+++ app/mailers/ziltag_devise_mailer.rb
@@ -3,10 +3,10 @@ class ZiltagDeviseMailer < Devise::Mailer
 
   def confirmation_instructions(record, token, opts = {})
     @token = token
-    @username = record.username
-    @box = record.box
     template = confirmation_template record
     initialize_from_record record
+    @box = @resource.box if @resource.is_a? ContentProvider
+    @partner_link = !@resource.is_a?(ContentProvider)
     mail headers_for(__method__, opts) do |format|
       format.html do
         render_inline_css template
