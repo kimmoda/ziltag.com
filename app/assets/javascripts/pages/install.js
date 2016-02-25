348ffd4a3726a87bffe24805a5fdd86b618a3307
Jian Weihang <tonytonyjan@gmail.com>
2016-02-25 22:26:18 +0800

    use the real path provided by webpack [#338]

--- app/assets/javascripts/pages/install.js
+++ app/assets/javascripts/pages/install.js
@@ -1,9 +1,12 @@
 import Tracker from '../tracker'
 import ZeroClipboard from 'zeroclipboard'
-import 'zeroclipboard/dist/ZeroClipboard.swf'
+import swfPath from 'zeroclipboard/dist/ZeroClipboard.swf'
 
 var btn = document.getElementById('copy-button')
 if (btn != undefined) {
+  ZeroClipboard.config({
+    swfPath: swfPath
+  })
   var client = new ZeroClipboard(document.getElementById('copy-button'))
   client.on('aftercopy', function(event) {
     Tracker.record('click-copy')
