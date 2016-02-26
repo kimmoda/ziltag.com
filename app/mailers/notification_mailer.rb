0543211764c1fe7d7807c70072f00cc4c3240446
Jian Weihang <tonytonyjan@gmail.com>
2016-02-26 21:33:12 +0800

    add new ziltag mailer [#270]

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,4 +1,12 @@
 class NotificationMailer < ApplicationMailer
+  def new_ziltag_notification(user, ziltag)
+    @user, @ziltag = user, ziltag
+    @unsubscribe_token = Unsubscribe.new(@user, @ziltag).token
+    mail to: @user.email do |format|
+      format.html{ render_inline_css __method__ }
+    end
+  end
+
   def new_comment_notification(user, comment)
     @user = user
     @comment = comment
--- test/mailers/previews/notification_preview.rb
+++ test/mailers/previews/notification_preview.rb
@@ -2,4 +2,8 @@ class NotificationPreview < ActionMailer::Preview
   def new_comment_notification
     NotificationMailer.new_comment_notification(User.first, Comment.first)
   end
+
+  def new_ziltag_notification
+    NotificationMailer.new_ziltag_notification(User.first, Ziltag.first)
+  end
 end
