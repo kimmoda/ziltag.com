1c4ad9411a05214012b75a6f61a31d40bd5cbdd1
Jian Weihang <tonytonyjan@gmail.com>
2016-02-28 15:31:21 +0800

    fix test [ci skip]

--- test/models/box_test.rb
+++ test/models/box_test.rb
@@ -1,26 +1,26 @@
 require 'test_helper'
 
 class BoxTest < ActiveSupport::TestCase
-  # def test_service
-  #   assert_equal 'tumblr', Box.new(url: 'http://weihang.tumblr.com').service
-  #   assert_equal 'wordpress', Box.new(url: 'http://weihang.wordpress.com').service
-  #   assert_equal 'pixnet', Box.new(url: 'http://weihang.pixnet.net').service
-  #   assert_equal 'xuite', Box.new(url: 'http://blog.xuite.net/foo/bar').service
-  #   assert_equal 'logdown', Box.new(url: 'http://weihang.logdown.com').service
-  #   assert_equal 'blogger', Box.new(url: 'http://weihang.blogspot.tw').service
-  #   assert_equal 'blogger', Box.new(url: 'http://weihang.blogspot.jp').service
-  #   assert_equal nil, Box.new(url: 'http://tonytonyjan.net').service
-  #   assert_equal nil, Box.new(url: '').service
-  # end
-  #
-  # def test_match_href
-  #   box = Box.new(url: 'http://weihang.blogspot.tw')
-  #   assert box.match_href? 'http://weihang.blogspot.tw/foo/bar'
-  #   assert box.match_href? 'http://weihang.blogspot.jp/foo/bar'
-  #   refute box.match_href? 'http://jian.blogspot.jp/foo/bar'
-  #   box = Box.new(url: 'http://weihang.tumblr.com')
-  #   assert box.match_href? 'http://weihang.tumblr.com/foo/bar'
-  # end
+  def test_service
+    assert_equal 'tumblr', Box.new(url: 'http://weihang.tumblr.com').service
+    assert_equal 'wordpress', Box.new(url: 'http://weihang.wordpress.com').service
+    assert_equal 'pixnet', Box.new(url: 'http://weihang.pixnet.net').service
+    assert_equal 'xuite', Box.new(url: 'http://blog.xuite.net/foo/bar').service
+    assert_equal 'logdown', Box.new(url: 'http://weihang.logdown.com').service
+    assert_equal 'blogger', Box.new(url: 'http://weihang.blogspot.tw').service
+    assert_equal 'blogger', Box.new(url: 'http://weihang.blogspot.jp').service
+    assert_equal nil, Box.new(url: 'http://tonytonyjan.net').service
+    assert_equal nil, Box.new(url: '').service
+  end
+
+  def test_match_href
+    box = Box.new(url: 'http://weihang.blogspot.tw')
+    assert box.match_href? 'http://weihang.blogspot.tw/foo/bar'
+    assert box.match_href? 'http://weihang.blogspot.jp/foo/bar'
+    refute box.match_href? 'http://jian.blogspot.jp/foo/bar'
+    box = Box.new(url: 'http://weihang.tumblr.com')
+    assert box.match_href? 'http://weihang.tumblr.com/foo/bar'
+  end
 
   def test_match_href_without_protocol
     box = Box.new(url: 'weihang.blogspot.tw')
