b360a31caadbcf8007bc1eb289164deb76110d56
Jian Weihang <tonytonyjan@gmail.com>
2016-02-29 17:57:21 +0800

    prevent send owner's ziltag notification to himself/herself.

--- app/interactors/notify_of_ziltag.rb
+++ app/interactors/notify_of_ziltag.rb
@@ -5,6 +5,7 @@ class NotifyOfZiltag
 
   def call
     user = @ziltag.photo.box.user
+    return if user == @ziltag.user
     unless @ziltag.unsubscribers.include?(user.id)
       NotificationMailer.new_ziltag_notification(user, @ziltag).deliver_later
     end
