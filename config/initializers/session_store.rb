3ab007aa3c18701eeb6d20507e6bf43fd9a26d41
Jian Weihang <tonytonyjan@gmail.com>
2016-03-03 13:21:44 +0800

    isolate session key between staging and production, close #352

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
 if Rails.env.production?
-  Rails.application.config.session_store :cookie_store, key: '_ziltag_session', domain: "#{Settings.host}"
+  Rails.application.config.session_store :cookie_store, key: Settings.session_key, domain: Settings.host
 else
   Rails.application.config.session_store :cookie_store, key: '_ziltag_session'
 end
