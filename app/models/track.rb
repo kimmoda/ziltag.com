656a7ced29b0ac59159e8d0aa74592199998b0c2
Jian Weihang <tonytonyjan@gmail.com>
2016-03-21 00:34:27 +0800

    add tracking stats information

--- app/models/track.rb
+++ app/models/track.rb
@@ -1,5 +1,6 @@
 class Track < ActiveRecord::Base
   # scopes
+  scope :stats_of_this_week, -> { where('created_at >= ?', 1.week.ago).group('date(created_at)', :event).count }
 
   # constants
 
@@ -12,5 +13,4 @@ class Track < ActiveRecord::Base
   # callbacks
 
   # other
-
 end
