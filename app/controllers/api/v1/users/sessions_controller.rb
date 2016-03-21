1eafed30f32f8e3a3023339693ba2fb261ccbd3e
Jian Weihang <tonytonyjan@gmail.com>
2016-03-22 01:38:33 +0800

    rename

--- app/controllers/api/v1/users/sessions_controller.rb
+++ app/controllers/api/v1/users/sessions_controller.rb
@@ -4,7 +4,7 @@ class Api::V1::Users::SessionsController < Devise::SessionsController
   respond_to :json
 
   def create
-    @user = User.find_for_database_authentication(login: params[:user][:login])
+    @user = User.find_for_database_authentication(login: params[:user][:sign_in])
     if @user && @user.valid_password?(params[:user][:password])
       sign_in(@user)
       render
