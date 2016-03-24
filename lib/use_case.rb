2cfb9a4bb73e5a91f11424546f86867d3c3cb1a4
Jian Weihang <tonytonyjan@gmail.com>
2016-03-24 10:50:17 +0800

    implement use case module to reduce logic from active record model

---
+++ lib/use_case.rb
@@ -0,0 +1,41 @@
+#    class AddToCart
+#      include UseCase
+#      def call cart, product
+#        # errors[:base] << 'something wrong'
+#        # results[:attr] = "value"
+#      end
+#    end
+#
+#    add_to_cart = AddToCart.call(cart, product)
+#    if add_to_cart.success?
+#      add_to_cart[:attr] # "value"
+#    else
+#      puts add_to_cart.errors
+#    end
+module UseCase
+  def self.included(mod)
+    mod.extend ClassMethods
+  end
+
+  module ClassMethods
+    def call *args
+      new(*args).tap(&:call)
+    end
+  end
+
+  def call
+    raise NotImplementedError
+  end
+
+  def results
+    @results ||= {}
+  end
+
+  def errors
+    @errors ||= {}
+  end
+
+  def success?
+    errors.empty?
+  end
+end
---
+++ test/units/use_case_test.rb
@@ -0,0 +1,42 @@
+require 'test_helper'
+require 'use_case'
+
+class Greeting
+  include UseCase
+
+  def initialize(name)
+    @name = name
+  end
+
+  def call
+    if @name.empty?
+      errors[:name] = 'name is empty'
+    else
+      results[:message] = "Hello, #{@name}"
+    end
+  end
+end
+
+class EmptyCase
+  include UseCase
+end
+
+class UseCaseTest < MiniTest::Unit::TestCase
+  def test_not_implemented_error
+    assert_raises NotImplementedError do
+      EmptyCase.call
+    end
+  end
+
+  def test_success
+    greeting = Greeting.call('Jian Weihang')
+    assert greeting.success?
+    assert_equal 'Hello, Jian Weihang', greeting.results[:message]
+  end
+
+  def test_error
+    greeting = Greeting.call('')
+    refute greeting.success?
+    assert_equal 'name is empty', greeting.errors[:name]
+  end
+end
