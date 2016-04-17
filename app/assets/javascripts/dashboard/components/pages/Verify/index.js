621b51882c687b97828f75732bb9776a4a643c5e
Jian Weihang <tonytonyjan@gmail.com>
2016-04-17 12:56:37 +0800

    form initialize

--- app/assets/javascripts/dashboard/components/pages/Verify/index.js
+++ app/assets/javascripts/dashboard/components/pages/Verify/index.js
@@ -1,24 +1,27 @@
-import {CheerBoard} from '../../../components/MessageBoard'
+import { CheerBoard } from '../../../components/MessageBoard'
 import Field from '../../../components/Field'
 import Button from '../../../components/Button'
 import './index.scss'
 import React from 'react'
+import { reduxForm } from 'redux-form';
+import { connect } from 'react-redux'
 
-export default class Verify extends React.Component {
+class Verify extends React.Component {
   render () {
+    const {fields: {password, password_confirmation, confirmation_token}, onSubmit} = this.props
     return(
       <div className="ziltag-verify">
         <CheerBoard />
-        <form className="ziltag-verify__form">
+        <form className="ziltag-verify__form" onSubmit={onSubmit} >
           <div className="ziltag-verify__fields">
             <div className="ziltag-verify__field">
-              <Field placeholder="Password" name="password" />
+              <Field placeholder="Password" {...password} />
             </div>
             <div className="ziltag-verify__field">
-              <Field placeholder="Confirm password" name="password_confirmation"/>
+              <Field placeholder="Confirm password" {...password_confirmation} />
             </div>
           </div>
-          <input type="text" name="confirmation_token" />
+          <input type="hidden" {...confirmation_token} />
           <div className="ziltag-verify__button">
             <Button>OK</Button>
           </div>
@@ -27,3 +30,18 @@ export default class Verify extends React.Component {
     )
   }
 }
+
+const mapStateToProps = (state, ownProps) => {
+  return {
+    initialValues: {
+      confirmation_token: ownProps.location.query.confirmation_token
+    }
+  }
+}
+
+Verify = reduxForm({
+  form: 'verify',
+  fields: ['password', 'password_confirmation', 'confirmation_token']
+}, mapStateToProps)(Verify)
+
+export default Verify
