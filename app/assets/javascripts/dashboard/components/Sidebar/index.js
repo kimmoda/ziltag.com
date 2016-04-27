1ab2db60c0b08beb4712efec371d21c2fb6f602a
Jian Weihang <tonytonyjan@gmail.com>
2016-04-27 10:16:22 +0800

    material design sidebar menu [#403]

--- app/assets/javascripts/dashboard/components/Sidebar/index.js
+++ app/assets/javascripts/dashboard/components/Sidebar/index.js
@@ -1,9 +1,11 @@
 import SearchField from '../SearchField'
-import {default as Menu, MenuItem} from '../Menu'
 import Footer from '../Footer'
 import './index.scss'
 
 import React from 'react'
+import Menu from 'material-ui/Menu'
+import MenuItem from 'material-ui/MenuItem'
+import { Link } from 'react-router'
 
 export default class Sidebar extends React.Component {
   render () {
@@ -11,10 +13,28 @@ export default class Sidebar extends React.Component {
       <div className="ziltag-sidebar">
         <SearchField />
         <div className="ziltag-sidebar__menu">
-          <Menu>
-            <MenuItem to="/tags">Tags</MenuItem>
-            <MenuItem to="/comments">Comments</MenuItem>
-            <MenuItem to="/account">Account</MenuItem>
+          <Menu
+            autoWidth={false}
+            style={{display: 'table', width: '100%'}}
+            listStyle={{paddingTop: 0}}>
+            <MenuItem>
+              <Link
+                to="/tags"
+                className="ziltag-sidebar__link"
+                activeClassName="ziltag-sidebar__link--active">Tags</Link>
+            </MenuItem>
+            <MenuItem>
+              <Link
+                to="/comments"
+                className="ziltag-sidebar__link"
+                activeClassName="ziltag-sidebar__link--active">Comments</Link>
+            </MenuItem>
+            <MenuItem>
+              <Link
+                to="/account"
+                className="ziltag-sidebar__link"
+                activeClassName="ziltag-sidebar__link--active">Account</Link>
+            </MenuItem>
           </Menu>
         </div>
         <div className="ziltag-sidebar__footer">
