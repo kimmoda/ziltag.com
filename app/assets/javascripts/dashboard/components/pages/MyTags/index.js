c983965d92c265a4c3fc658f6981b8fe2fc14dca
Jian Weihang <tonytonyjan@gmail.com>
2016-04-30 23:56:18 +0800

    show images [#394]

--- app/assets/javascripts/dashboard/components/pages/MyTags/index.js
+++ app/assets/javascripts/dashboard/components/pages/MyTags/index.js
@@ -1,11 +1,21 @@
 import PartnerNavBar from '../../PartnerNavBar'
+import * as actions from '../../../actions'
 import './index.scss'
 
 import React from 'react'
 import Masonry from 'react-masonry-component'
+import { connect } from 'react-redux'
 
-export default class MyTags extends React.Component {
+class MyTags extends React.Component {
   render () {
+    const { ziltagMaps, onClickZiltagMap } = this.props
+    const imageElements = ziltagMaps.map((ziltagMap)=>{
+      return(
+        <div key={ziltagMap.id} className="ziltag-my-tags__image" onClick={()=>{onClickZiltagMap(ziltagMap.id)}}>
+          <img width="100%" src={ziltagMap.src} />
+        </div>
+      )
+    })
     return (
       <div className="ziltag-my-tags">
         <PartnerNavBar />
@@ -14,9 +24,24 @@ export default class MyTags extends React.Component {
           <Masonry
             className="ziltag-my-tags__images-container"
             options={{columnWidth: 253}}>
+            {imageElements}
           </Masonry>
         </div>
       </div>
     )
   }
 }
+
+function mapStateToProps(state) {
+  return {
+    ziltagMaps: state.me ? state.me.websites[state.siteMenu.selected].maps_without_tags : []
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    onClickZiltagMap: (ziltagMapID) => dispatch(actions.openIframeModal(`/ziltag_maps/${ziltagMapID}`))
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(MyTags)
