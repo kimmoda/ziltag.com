36854fa5075a65c2347c46dd3a56eb040b8688d7
Jian Weihang <tonytonyjan@gmail.com>
2016-05-18 17:00:57 +0800

    add feature component

--- app/assets/javascripts/dashboard/components/pages/Landing/index.js
+++ app/assets/javascripts/dashboard/components/pages/Landing/index.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from '../../Header'
 import Cover from './components/Cover'
+import Feature from './components/Feature'
 import './index.scss'
 
 export default props => (
   <div className="ziltag-landing">
     <div className="ziltag-landing__header"><Header/></div>
     <Cover />
+    <Feature />
   </div>
 )
