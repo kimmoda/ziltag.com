0013cd447b586f30d03d436eafe0c3bccfdad3e7
Jian Weihang <tonytonyjan@gmail.com>
2016-05-29 23:26:08 +0800

    fix test

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -1,11 +1,6 @@
 require 'test_helper'
 
 class PagesControllerTest < ActionController::TestCase
-  def test_get_home
-    get :home
-    assert_response :success
-  end
-
   class SignInContext < self
     def setup
       sign_in users(:tony)
