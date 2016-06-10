b764f842e524fa8cb76d01647492862747983a09
Jian Weihang <tonytonyjan@gmail.com>
2016-06-10 10:10:22 +0800

    new verified page [#443]

--- app/assets/javascripts/dashboard/components/pages/Verified/index.js
+++ app/assets/javascripts/dashboard/components/pages/Verified/index.js
@@ -1,12 +1,14 @@
-import './index.scss'
-import { CheerBoard } from '../../../components/MessageBoard'
+import Cheer from '../../../components/Cheer'
 import React from 'react'
+import './index.scss'
 
 export default class Verified extends React.Component {
   render () {
     return (
       <div className="ziltag-verified">
-        <CheerBoard title="Password successfully setup." subtitle="We will lead you to your dashboard now."/>
+        <div className="ziltag-verified__cheer">
+          <Cheer title="Password setup successfully" subtitle="Going to dashboard now."/>
+        </div>
       </div>
     )
   }
