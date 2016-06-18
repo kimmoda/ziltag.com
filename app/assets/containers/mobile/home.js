94e2854d6ba2ef449cac4a0d03c67f78f45e1a16
Jian Weihang <tonytonyjan@gmail.com>
2016-06-18 14:55:15 +0800

    remove useless codes

--- app/assets/containers/mobile/home.js
+++
@@ -1,8 +0,0 @@
-import {connect} from 'react-redux'
-import Home from '../../components/mobile/home'
-
-function mapStateToProps(state) {
-  return {modal: state.modal}
-}
-
-export default connect(mapStateToProps)(Home)
--- app/assets/javascripts/blocks.js
+++
@@ -1,2 +0,0 @@
-import './blocks/profile.coffee'
-import './blocks/platform.coffee'
