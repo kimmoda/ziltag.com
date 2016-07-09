acb267db12dbf5846edc21900272055e0df2f0c0
Jian Weihang <tonytonyjan@gmail.com>
2016-07-09 16:59:09 +0800

    fix registration contorller

--- app/controllers/api/v1/users/registrations_controller.rb
+++ app/controllers/api/v1/users/registrations_controller.rb
@@ -9,6 +9,7 @@ class Api::V1::Users::RegistrationsController < Devise::RegistrationsController
     resource.save
     yield resource if block_given?
     if resource.persisted?
+      SendWelcomeEmailJob.perform_later(resource)
       if resource.active_for_authentication?
         set_flash_message :notice, :signed_up if is_flashing_format?
         sign_up(resource_name, resource)
--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,29 +3,6 @@ class RegistrationsController < Devise::RegistrationsController
   before_action :set_headers
   before_action :set_back_url
 
-  def create
-    build_resource(sign_up_params)
-
-    resource.save
-    yield resource if block_given?
-    if resource.persisted?
-      SendWelcomeEmailJob.perform_later(resource)
-      if resource.active_for_authentication?
-        set_flash_message! :notice, :signed_up
-        sign_up(resource_name, resource)
-        respond_with resource, location: after_sign_up_path_for(resource)
-      else
-        set_flash_message! :notice, :"signed_up_but_#{resource.inactive_message}"
-        expire_data_after_sign_in!
-        respond_with `resource`, location: after_inactive_sign_up_path_for(resource)
-      end
-    else
-      clean_up_passwords resource
-      set_minimum_password_length
-      respond_with resource
-    end
-  end
-
 protected
 
   def update_resource(resource, params)
