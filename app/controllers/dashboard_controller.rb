37ad4f3c22fae354213ed4bb2b07a5d5422550bb
Jian Weihang <tonytonyjan@gmail.com>
2016-08-01 00:57:59 +0800

    track visit home close #507

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,6 +2,8 @@ class DashboardController < ApplicationController
   def index
     if request.path.start_with?('/dashboard') && !request.path.start_with?('/dashboard/verify')
       authenticate_user!
+    else
+      track 'visit-home'
     end
     render layout: false
   end
