6df1b641337cbebf648a242346ad164a826e26ac
Jian Weihang <tonytonyjan@gmail.com>
2016-08-20 17:15:16 +0800

    SendProductFeedbackToGeneralUser

--- app/jobs/send_product_feedback_to_general_user_job.rb
+++ app/jobs/send_product_feedback_to_general_user_job.rb
@@ -3,7 +3,7 @@ class SendProductFeedbackToGeneralUserJob < ActiveJob::Base #:nodoc:
   queue_as :default
 
   def perform(user)
-    error = SendProductFeedbackToGeneralUser.call(user).context[:error]
+    error = SendProductFeedbackToGeneralUser.perform(user).error
     raise error if error
   end
 end
