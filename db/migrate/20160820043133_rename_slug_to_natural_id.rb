3ab1dea67bb63a12c5ac1dc860bcc7e217cb97f6
Jian Weihang <tonytonyjan@gmail.com>
2016-08-22 12:01:53 +0800

    typo

--- db/migrate/20160820043133_rename_slug_to_natural_id.rb
+++ db/migrate/20160820043133_rename_slug_to_natural_id.rb
@@ -1,6 +1,6 @@
 class RenameSlugToNaturalId < ActiveRecord::Migration
   def change
-    rename_column :photos, :natural_id, :natural_id
-    rename_column :ziltags, :natural_id, :natural_id
+    rename_column :photos, :slug, :natural_id
+    rename_column :ziltags, :slug, :natural_id
   end
 end
