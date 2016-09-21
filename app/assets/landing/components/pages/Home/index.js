cd9e31632e66d3f460c8a0046fda4e8e9d66f740
Jian Weihang <tonytonyjan@gmail.com>
2016-09-21 23:51:34 +0800

    footer

--- app/assets/landing/components/pages/Home/index.js
+++ app/assets/landing/components/pages/Home/index.js
@@ -7,6 +7,14 @@ export default () => (
     <div className="p-home__cover"></div>
     <div className="p-home__usage"></div>
     <div className="p-home__get-start"></div>
-    <div className="p-home__footer"></div>
+    <div className="p-home__footer">
+      <div className="p-home__copyright">© Ziltag Inc., 2016. All Rights Reserved.</div>
+      <ul className="p-home__links">
+        <li className="p-home__link"><a href="http://blog.ziltag.com/about" target="_blank" ref="noopener">About</a></li>
+        <li className="p-home__link"><a href="http://blog.ziltag.com" target="_blank" ref="noopener">Blog</a></li>
+        <li className="p-home__link"><a href="http://blog.ziltag.com/terms" target="_blank" ref="noopener">Terms</a></li>
+        <li className="p-home__link"><a href="http://blog.ziltag.com/privacy/" target="_blank" ref="noopener">Privacy</a></li>
+      </ul>
+    </div>
   </div>
 )
