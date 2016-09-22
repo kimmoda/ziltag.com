57eb4ac08be5815e61569fedb32d139216b8fa10
Jian Weihang <tonytonyjan@gmail.com>
2016-09-22 17:29:39 +0800

    typo

--- app/assets/landing/components/pages/Home/index.js
+++ app/assets/landing/components/pages/Home/index.js
@@ -23,10 +23,10 @@ export default () => (
     <div className="p-home__footer">
       <div className="p-home__copyright">© Ziltag Inc., 2016. All Rights Reserved.</div>
       <ul className="p-home__links">
-        <li className="p-home__link"><a href="http://blog.ziltag.com/about" target="_blank" ref="noopener">About</a></li>
-        <li className="p-home__link"><a href="http://blog.ziltag.com" target="_blank" ref="noopener">Blog</a></li>
-        <li className="p-home__link"><a href="http://blog.ziltag.com/terms" target="_blank" ref="noopener">Terms</a></li>
-        <li className="p-home__link"><a href="http://blog.ziltag.com/privacy/" target="_blank" ref="noopener">Privacy</a></li>
+        <li className="p-home__link"><a href="http://blog.ziltag.com/about" target="_blank" rel="noopener">About</a></li>
+        <li className="p-home__link"><a href="http://blog.ziltag.com" target="_blank" rel="noopener">Blog</a></li>
+        <li className="p-home__link"><a href="http://blog.ziltag.com/terms" target="_blank" rel="noopener">Terms</a></li>
+        <li className="p-home__link"><a href="http://blog.ziltag.com/privacy/" target="_blank" rel="noopener">Privacy</a></li>
       </ul>
     </div>
   </div>
