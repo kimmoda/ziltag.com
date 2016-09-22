4479bea57000d59bcbab133f7404df7b7e5b4245
Jian Weihang <tonytonyjan@gmail.com>
2016-09-22 16:17:52 +0800

    get start section

--- app/assets/landing/components/pages/Home/index.js
+++ app/assets/landing/components/pages/Home/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Button from 'ziltag-elements/dist/Button'
 
 require('./index.css')
 
@@ -10,7 +11,8 @@ export default () => (
     <div className="p-home__usage"></div>
     <div className="p-home__get-start">
       <div className="p-home__title">Get Started Now.</div>
-      <div className="p-home__code"></div>
+      <div style={{marginTop: 26}}><Button width={164} text="Sign Up"/></div>
+      <div style={{marginTop: 16}}><Button width={164} text="Watch Video" color="gray"/></div>
     </div>
     <div className="p-home__footer">
       <div className="p-home__copyright">© Ziltag Inc., 2016. All Rights Reserved.</div>
