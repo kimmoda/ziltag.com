b3a84c72be0926528529e1bf637e820ebdaf367c
Jian Weihang <tonytonyjan@gmail.com>
2016-10-19 02:23:13 +0800

    remove unused module

--- app/assets/landing/routes/index.js
+++ app/assets/landing/routes/index.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import {Router, IndexRoute, Route, browserHistory} from 'react-router'
-import {I18nextProvider} from 'react-i18next'
 import Home from './Home'
 
 export const getRoutes = (history=browserHistory) => (
