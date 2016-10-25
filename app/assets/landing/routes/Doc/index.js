10ff85115867d14656afb739227afc428bc28cfd
Jian Weihang <tonytonyjan@gmail.com>
2016-10-25 15:40:54 +0800

    add doc page

---
+++ app/assets/landing/routes/Doc/index.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import Modal from 'Modal'
+import ModalButton from 'ModalButton'
+import translate from 'hoc/translate'
+import {Link} from 'react-router'
+
+import './index.css'
+
+export default translate(props => {
+  const {t} = props
+  return (
+    <div>
+      <Link to="/"><div className="p-doc__logo"/></Link>
+      <div className="p-doc__buttons">
+        <ModalButton text={t('sign_up')} width={76} modalName="signUp" style={{fontSize: 14}}/>
+        <ModalButton text={t('sign_in')} width={76} color="gray" modalName="signIn" style={{fontSize: 14, marginLeft: 18}}/>
+      </div>
+      <div className="p-doc__body">
+        <h1>Plugin Configuration</h1>
+        <h2>Attributes</h2>
+        <h3>Disable</h3>
+        <p>To disable Ziltag on specific image, add a <code>data-ziltag=&quot;false&quot;</code> attribute on the <code>img</code>.</p>
+        <p><code>&lt;img src=&quot;...src&quot; data-ziltag=&quot;false&quot;/&gt;</code></p>
+        <h3>Autoplay</h3>
+        <p><code>&lt;img src=&quot;...src&quot; data-ziltag-autoplay=&quot;true|false&quot;/&gt;</code></p>
+        <p>default: <code>true</code></p>
+        <h3>Enable Switch</h3>
+        <p><code>&lt;img src=&quot;...src&quot; data-ziltag-switch=&quot;true|false&quot;/&gt;</code></p>
+        <p>default: <code>true</code></p>
+        <h2>Limits</h2>
+        <p>The <code>img</code>&#39;s width must be greater than or equal to <code>200px</code>, height must be greater than or equal to <code>100px</code>.</p>
+      </div>
+      <Modal/>
+    </div>
+  )
+})
--- app/assets/landing/routes/index.js
+++ app/assets/landing/routes/index.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import {Router, IndexRoute, Route, browserHistory} from 'react-router'
 import Home from './Home'
+import Doc from './Doc'
 
 export const getRoutes = (history=browserHistory) => (
   <Router history={browserHistory}>
     <Route path="/" component={Home}/>
+    <Route path="/doc" component={Doc}/>
   </Router>
 )
