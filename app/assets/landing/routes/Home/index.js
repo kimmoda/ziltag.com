cacedf972b3953720993f8708e7e4134fab12476
Jian Weihang <tonytonyjan@gmail.com>
2016-11-04 21:11:32 +0800

    using JS to adjust cover image

--- app/assets/landing/routes/Home/index.js
+++ app/assets/landing/routes/Home/index.js
@@ -20,55 +20,68 @@ const example = `<img src="sample.jpg">
   data-ziltag="YOUR_TOKEN">
 </script>`
 
-const Home = props => {
-  const {t, isSignedIn} = props
-  return (
-    <div className="p-home">
-      <div className="p-home__cover">
-        <img className="p-home__cover-image" src={require('./cat.jpg')} data-ziltag-switch="false"/>
-        <div className="p-home__logo"/>
-        <div className="p-home__auth-buttons">
-          <Link to="/doc" style={{textDecoration: 'none', color: 'white', fontSize: 14, fontWeight: 500}}>{t('doc')}</Link>
-            {isSignedIn || <ModalButton text={t('sign_up')} width={76} modalName="signUp" style={{fontSize: 14}}/>}
-            {isSignedIn || <ModalButton text={t('sign_in')} width={76} color="gray" modalName="signIn" style={{fontSize: 14}}/>}
-            {isSignedIn && <a href="/dashboard/account/"><Button style={{fontSize: 14}} text={t('dashboard')}/></a>}
+class Home extends React.Component {
+
+  componentDidMount() {
+    if (window.innerWidth/window.innerHeight > this._cover.naturalWidth/this._cover.naturalHeight) {
+      this._cover.style.width = window.innerWidth + 'px'
+      this._cover.style.top = -Math.abs(window.innerHeight - this._cover.height)/2 + 'px'
+    } else {
+      this._cover.style.height = window.innerHeight + 'px'
+      this._cover.style.left = -Math.abs(window.innerWidth - this._cover.width)/2 + 'px'
+    }
+  }
+
+  render() {
+    const {t, isSignedIn} = this.props
+    return (
+      <div className="p-home">
+        <div className="p-home__cover">
+          <img ref={c=>{this._cover = c}} className="p-home__cover-image" src={require('./cat.jpg')} data-ziltag-switch="false"/>
+          <div className="p-home__logo"/>
+          <div className="p-home__auth-buttons">
+            <Link to="/doc" style={{textDecoration: 'none', color: 'white', fontSize: 14, fontWeight: 500}}>{t('doc')}</Link>
+              {isSignedIn || <ModalButton text={t('sign_up')} width={76} modalName="signUp" style={{fontSize: 14}}/>}
+              {isSignedIn || <ModalButton text={t('sign_in')} width={76} color="gray" modalName="signIn" style={{fontSize: 14}}/>}
+              {isSignedIn && <a href="/dashboard/account/"><Button style={{fontSize: 14}} text={t('dashboard')}/></a>}
+          </div>
+          <div className="p-home__intro">
+            <div style={{textAlign: 'center'}}><div className="p-home__title">{t('title')}</div></div>
+            <div style={{textAlign: 'center', marginTop: 14}}><div className="p-home__subtitle">{t('subtitle')}</div></div>
+            <div style={{marginTop: 45}}><DemoForm/></div>
+          </div>
         </div>
-        <div className="p-home__intro">
-          <div style={{textAlign: 'center'}}><div className="p-home__title">{t('title')}</div></div>
-          <div style={{textAlign: 'center', marginTop: 14}}><div className="p-home__subtitle">{t('subtitle')}</div></div>
-          <div style={{marginTop: 45}}><DemoForm/></div>
+        <div className="p-home__usage">
+          <div className="p-home__usage-example">
+            <div className="p-home__title" style={{textAlign: 'center'}}>{t('dead_simple_install')}</div>
+            <pre className="p-home__code hljs html" dangerouslySetInnerHTML={{__html: Highlight.highlight('html', example).value}}/>
+          </div>
+          <div><img className="p-home__sample-image" src={require('./sample.jpg')}/></div>
         </div>
-      </div>
-      <div className="p-home__usage">
-        <div className="p-home__usage-example">
-          <div className="p-home__title" style={{textAlign: 'center'}}>{t('dead_simple_install')}</div>
-          <pre className="p-home__code hljs html" dangerouslySetInnerHTML={{__html: Highlight.highlight('html', example).value}}/>
+        <div className="p-home__get-start">
+          <div style={{textAlign: 'center'}}><div className="p-home__title">{t('get_started_now')}</div></div>
+          <div style={{marginTop: 26}}><ModalButton width={164} text={t('join_now')} modalName="signUp"/></div>
+          <div style={{marginTop: 16}}><ModalButton width={164} text={t('watch_video')} color="gray" modalName="video" modalMode="center"/></div>
         </div>
-        <div><img className="p-home__sample-image" src={require('./sample.jpg')}/></div>
+        <div className="p-home__footer">
+          <div className="p-home__copyright">© Ziltag Inc., 2016. All Rights Reserved.</div>
+          <ul className="p-home__links">
+            <li className="p-home__link"><a href="http://blog.ziltag.com" target="_blank" rel="noopener">{t('blog')}</a></li>
+            <li className="p-home__link"><a href="https://help.ziltag.com" target="_blank" rel="noopener">{t('support')}</a></li>
+            <li className="p-home__link"><a href="https://ziltag.zendesk.com/hc/en-us/requests/new" target="_blank" rel="noopener">{t('suggest_new_features')}</a></li>
+            <li className="p-home__link"><a href="http://blog.ziltag.com/terms" target="_blank" rel="noopener">{t('terms')}</a></li>
+            <li className="p-home__link"><a href="http://blog.ziltag.com/privacy/" target="_blank" rel="noopener">{t('privacy')}</a></li>
+          </ul>
+          <ul className="p-home__icon-links">
+            <li className="p-home__icon-link p-home__icon-link--github"><a href="https://github.com/ziltag" target="_blank" rel="noopener"></a></li>
+            <li className="p-home__icon-link p-home__icon-link--twitter"><a href="https://twitter.com/ziltagapp" target="_blank" rel="noopener"></a></li>
+          </ul>
+        </div>
+        <Demo/>
+        <Modal/>
       </div>
-      <div className="p-home__get-start">
-        <div style={{textAlign: 'center'}}><div className="p-home__title">{t('get_started_now')}</div></div>
-        <div style={{marginTop: 26}}><ModalButton width={164} text={t('join_now')} modalName="signUp"/></div>
-        <div style={{marginTop: 16}}><ModalButton width={164} text={t('watch_video')} color="gray" modalName="video" modalMode="center"/></div>
-      </div>
-      <div className="p-home__footer">
-        <div className="p-home__copyright">© Ziltag Inc., 2016. All Rights Reserved.</div>
-        <ul className="p-home__links">
-          <li className="p-home__link"><a href="http://blog.ziltag.com" target="_blank" rel="noopener">{t('blog')}</a></li>
-          <li className="p-home__link"><a href="https://help.ziltag.com" target="_blank" rel="noopener">{t('support')}</a></li>
-          <li className="p-home__link"><a href="https://ziltag.zendesk.com/hc/en-us/requests/new" target="_blank" rel="noopener">{t('suggest_new_features')}</a></li>
-          <li className="p-home__link"><a href="http://blog.ziltag.com/terms" target="_blank" rel="noopener">{t('terms')}</a></li>
-          <li className="p-home__link"><a href="http://blog.ziltag.com/privacy/" target="_blank" rel="noopener">{t('privacy')}</a></li>
-        </ul>
-        <ul className="p-home__icon-links">
-          <li className="p-home__icon-link p-home__icon-link--github"><a href="https://github.com/ziltag" target="_blank" rel="noopener"></a></li>
-          <li className="p-home__icon-link p-home__icon-link--twitter"><a href="https://twitter.com/ziltagapp" target="_blank" rel="noopener"></a></li>
-        </ul>
-      </div>
-      <Demo/>
-      <Modal/>
-    </div>
-  )
+    )
+  }
 }
 
 export default connect(
