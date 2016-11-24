5d5182f54860e53bbfa3ec1a063e2e97c4bdbe2c
Jian Weihang <tonytonyjan@gmail.com>
2016-11-24 14:43:26 +0800

    fix forgot password [#579]

--- app/assets/landing/saga/watchForgotPassword.js
+++ app/assets/landing/saga/watchForgotPassword.js
@@ -1,6 +1,6 @@
 import {takeEvery} from 'redux-saga'
 import {call, put} from 'redux-saga/effects'
-import {createShortenURL} from '../api'
+import {forgotPassword} from '../api'
 import {failForgotPassword, openModal} from '../actions'
 
 export default function* () {
