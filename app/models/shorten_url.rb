26d9ad06660b69131a343396826fda101afbe212
Jian Weihang <tonytonyjan@gmail.com>
2016-12-30 23:11:28 +0100

    update photo's namespace as well after updating shorten url [#610]

--- app/models/shorten_url.rb
+++ app/models/shorten_url.rb
@@ -11,6 +11,13 @@ class ShortenUrl < ActiveRecord::Base
   # validations
 
   # callbacks
+  after_update :update_photo_namespace, if: :natural_id_changed?
 
   # other
+
+  private
+
+  def update_photo_namespace
+    Photo.where(namespace: natural_id_was).update_all(namespace: natural_id)
+  end
 end
