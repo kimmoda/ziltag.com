2d0e3f9897d3cb458a0af7711361165e189bba1b
Jian Weihang <tonytonyjan@gmail.com>
2017-01-16 09:27:24 +0800

    refine wording [#625]

--- app/assets/javascripts/dashboard/components/Guide/index.js
+++ app/assets/javascripts/dashboard/components/Guide/index.js
@@ -22,10 +22,10 @@ export default props => (
         <TabPanel>
           <ol className="ziltag-guide__list" >
             <li className="ziltag-guide__list-item">Go to <b>Administration Screens</b> > <b>Plugins</b> > <b>Add New</b>.</li>
-            <li className="ziltag-guide__list-item">Upload the .zip file downloaded <a style={{color: '#008BF3'}} href="//github.com/ziltag/ziltag-wp/releases/latest" target="_blank">here</a>. Click Install Now.</li>
-            <li className="ziltag-guide__list-item">After the plugin is installed, click <b>Activate Plugin</b>.</li>
-            <li className="ziltag-guide__list-item">Click <b>Settings</b>.</li>
-            <li className="ziltag-guide__list-item">Enter your Plugin Token as shown above. </li>
+            <li className="ziltag-guide__list-item">Download the <a style={{color: '#008BF3'}} href="//github.com/ziltag/ziltag-wp/releases/latest" target="_blank">plugin</a> and click <b>Upload Plugin</b>. After uploading the file, click <b>Install Now</b>.</li>
+            <li className="ziltag-guide__list-item">When installed, go back to <b>Administration Screens</b> > <b>Plugins</b>. Find Ziltag Plugin and click <b>Activate</b>.</li>
+            <li className="ziltag-guide__list-item">Click <b>Settings</b> and enter your Token.</li>
+            <li className="ziltag-guide__list-item">Click <b>Save Changes</b>.</li>
           </ol>
         </TabPanel>
         <TabPanel>
